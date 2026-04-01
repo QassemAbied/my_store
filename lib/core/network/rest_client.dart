ecc7b491f827736625d2ebc2f2e54b03d19aa0ae
qassem abied <qassemab1@gmail.com>
2026-04-01 17:01:53 +0200

    add getProduct endpoint to RestClient

--- lib/core/network/rest_client.dart
+++ lib/core/network/rest_client.dart
@@ -2,9 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:my_store/core/network/api_contstants.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
+
+import '../../features/home/data/models/product_models.dart';
 part 'rest_client.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
 abstract class RestClient  {
 factory RestClient(Dio dio,{String baseUrl})= _RestClient;
+@GET("/products")
+  Future<ProductModels> getProduct();
 }
