39ddf178e62490a7fe1304738c2339025993f11b
qassem abied <qassemab1@gmail.com>
2026-04-01 15:32:46 +0200

    add RestClient using Retrofit for API requests

---
+++ lib/core/network/rest_client.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:my_store/core/network/api_contstants.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+part 'rest_client.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class RestClient  {
+factory RestClient(Dio dio,{String baseUrl})= _RestClient;
+}
