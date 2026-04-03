684aad339083596e3bf2d241cb85e4a05a8434cb
qassem abied <qassemab1@gmail.com>
2026-04-03 14:17:40 +0200

    add fixImageUrl method to ApiConstants to handle localhost URLs

--- lib/core/network/api_contstants.dart
+++ lib/core/network/api_contstants.dart
@@ -1,6 +1,9 @@
 class ApiConstants {
   static const Duration timeout= Duration(seconds: 30);
   static const String baseUrl= "http://10.0.2.2:9000";
+ static  String fixImageUrl(String url) {
+    return url.replaceAll("localhost", "10.0.2.2");
+  }
   static const String publishable="x-publishable-api-key";
   static const String apiKey="pk_ecbf59591d859787be74acff3df180060ed990228d9dba0e95bfdc25182a23e6";
 }
