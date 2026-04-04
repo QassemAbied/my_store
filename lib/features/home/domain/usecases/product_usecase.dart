3418ac6e9055cf3a0a5f8c01f0effb059a1e5796
qassem abied <qassemab1@gmail.com>
2026-04-04 14:24:27 +0200

    update ProductUseCase to accept ProductParams and adjust method call accordingly

--- lib/features/home/domain/usecases/product_usecase.dart
+++ lib/features/home/domain/usecases/product_usecase.dart
@@ -1,16 +1,17 @@
 import 'package:my_store/core/network/api_result.dart';
-import 'package:my_store/core/utils/use_case.dart';
+import 'package:my_store/core/network/use_case.dart';
 
 import '../entities/product_entities.dart';
+import '../entities/product_param.dart';
 import '../repositories/home_repositories.dart';
 
-class ProductUseCase extends UseCase<ProductResponseEntity, NoParams> {
+class ProductUseCase extends UseCase<ProductResponseEntity, ProductParams > {
   final HomeRepositories homeRepositories;
 
   ProductUseCase(this.homeRepositories);
 
   @override
-  Future<ApiResult<ProductResponseEntity>> call(NoParams params) async {
-    return await homeRepositories.getProducts();
+  Future<ApiResult<ProductResponseEntity>> call(ProductParams  params) async {
+    return await homeRepositories.getProducts(params);
   }
 }
