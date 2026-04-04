38df6e2fd31d2c0be2a553a5280e94aca4975b2a
qassem abied <qassemab1@gmail.com>
2026-04-04 14:23:28 +0200

    implement pagination in HomeCubit with limit and offset parameters

--- lib/features/home/presentation/cubit/home_cubit.dart
+++ lib/features/home/presentation/cubit/home_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../core/utils/use_case.dart';
+import 'package:my_store/features/home/domain/entities/product_entities.dart';
+import 'package:my_store/features/home/domain/entities/product_param.dart';
 import '../../domain/usecases/product_usecase.dart';
 import 'home_state.dart';
 
@@ -8,12 +9,38 @@ class HomeCubit extends Cubit<HomeState> {
 
   HomeCubit(this.productUseCase) : super(HomeInitial());
 
+  int limit = 7;
+   int offset = 0;
+   bool hasMore = true;
+   bool isLoading = false;
 
-  Future getProduct() async{
-    emit(ProductLoading());
-    final result =await productUseCase(NoParams());
+  List<ProductItemEntity> productList = [];
+
+
+  Future getProduct({ bool isLoadMore=false}) async{
+    if(isLoading || !hasMore) return;
+    isLoading=true;
+    if(!isLoadMore){
+       offset = 0;
+       hasMore = true;
+       productList.clear();
+       emit(ProductLoading());
+    }
+
+
+   final params=ProductParams(limit: limit, offset: offset, fields: "id,title,description,thumbnail");
+    final result =await productUseCase(params);
     result.result.fold((failure)=>emit(ProductFailure(failure.toString())),
-        (data)=>emit(ProductSuccess(data)));
-
+        (data){
+      final newProduct = data.products;
+      if(newProduct?.isEmpty??false){
+        hasMore=false;
+      }else{
+        productList.addAll(newProduct!);
+        offset+=limit;
+      }
+      emit(ProductSuccess(product: productList,));
+        });
+    isLoading = false;
   }
 }
