0a96a2176a2c9ac1451b854b633964920c976392
qassem abied <qassemab1@gmail.com>
2026-04-04 14:23:38 +0200

    update getProducts to accept ProductParams in HomeRepositories

--- lib/features/home/domain/repositories/home_repositories.dart
+++ lib/features/home/domain/repositories/home_repositories.dart
@@ -1,7 +1,9 @@
 import 'package:my_store/core/network/api_result.dart';
 import 'package:my_store/features/home/domain/entities/product_entities.dart';
 
+import '../entities/product_param.dart';
+
 abstract class HomeRepositories {
 
-  Future<ApiResult<ProductResponseEntity>> getProducts();
+  Future<ApiResult<ProductResponseEntity>> getProducts(ProductParams params);
 }
