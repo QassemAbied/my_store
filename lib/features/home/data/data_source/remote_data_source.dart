12193f24184fe134624a270bda92456ad77f2706
qassem abied <qassemab1@gmail.com>
2026-04-04 14:24:41 +0200

    update remote data source to support product parameters in getProducts method

--- lib/features/home/data/data_source/remote_data_source.dart
+++ lib/features/home/data/data_source/remote_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:my_store/features/home/data/models/product_models.dart';
 
+import '../../domain/entities/product_param.dart';
+
 abstract class RemoteDataSource {
-  Future<ProductResponseModel> getProducts();
+  Future<ProductResponseModel> getProducts(ProductParams params);
 }
--- lib/features/home/data/data_source/remote_data_source_impl.dart
+++ lib/features/home/data/data_source/remote_data_source_impl.dart
@@ -1,12 +1,13 @@
 import 'package:my_store/features/home/data/data_source/remote_data_source.dart';
 import 'package:my_store/features/home/data/models/product_models.dart';
 import '../../../../core/network/rest_client.dart';
+import '../../domain/entities/product_param.dart';
 
 class RemoteDataSourceImpl implements RemoteDataSource{
   final RestClient restClient;
   RemoteDataSourceImpl({required this.restClient});
   @override
-  Future<ProductResponseModel> getProducts()async {
-  return  restClient.getProduct("id,title,thumbnail",);
+  Future<ProductResponseModel> getProducts(ProductParams params)async {
+  return  restClient.getProduct(params.fields,params.limit,params.offset);
   }
  }
