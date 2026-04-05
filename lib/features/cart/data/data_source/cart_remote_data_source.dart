f47c07d012e8924419ea23aec8a7d46aa389fb9b
qassem abied <qassemab1@gmail.com>
2026-04-05 14:22:11 +0200

    implement CartRemoteDataSource and its implementation with methods for regions, cart creation, and cart items

---
+++ lib/features/cart/data/data_source/cart_remote_data_source.dart
@@ -0,0 +1,12 @@
+import 'package:my_store/features/cart/data/models/cart_item_model.dart';
+import 'package:my_store/features/cart/domain/entities/add_cart_request.dart';
+
+import '../models/regions_model.dart';
+
+abstract class CartRemoteDataSource {
+  Future<String> getRegions();
+  Future<String> createCart(Map<String, dynamic> body);
+
+  Future<CartResponseModel> getCartItems(String id);
+  // Future<void> addToCart(AddCartRequest request);
+}
