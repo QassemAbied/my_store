40a3fb4717f74760085e8a53fc314671944c75b8
qassem abied <qassemab1@gmail.com>
2026-04-05 18:09:21 +0200

    add parameters for cart operations: DeleteCartParams, AddCartRequest, and UpdateCartParams

---
+++ lib/features/cart/domain/entities/params.dart
@@ -0,0 +1,22 @@
+class DeleteCartParams {
+  final String cartId;
+  final String lineId;
+
+  DeleteCartParams(this.cartId, this.lineId);
+}
+
+class AddCartRequest {
+  final String cartId;
+  final Map<String, dynamic> body;
+
+  AddCartRequest(this.cartId, this.body);
+}
+
+class UpdateCartParams {
+  final String cartId;
+  final String lineId;
+  final Map<String, dynamic> body;
+
+
+  UpdateCartParams(this.cartId, this.lineId,  this.body);
+}
