9a2381d46f5bafec7decab131c1b7ccdb480305f
qassem abied <qassemab1@gmail.com>
2026-04-05 18:07:10 +0200

    refactor CreateCartUseCase and remove unused whitespace

--- lib/features/cart/domain/usecases/create_cart_use_case.dart
+++ lib/features/cart/domain/usecases/create_cart_use_case.dart
@@ -2,16 +2,12 @@ import '../../../../core/network/api_result.dart';
 import '../../../../core/network/use_case.dart';
 import '../repository.dart';
 
-class CreateCartUseCase extends UseCase<String,Map<String, dynamic>>{
+class CreateCartUseCase extends UseCase<String, Map<String, dynamic>> {
   final CartRepository _cartRepository;
   CreateCartUseCase(this._cartRepository);
 
   @override
-  Future<ApiResult<String>> call(Map<String, dynamic> body)async {
+  Future<ApiResult<String>> call(Map<String, dynamic> body) async {
     return await _cartRepository.createCart(body);
-
-
-
   }
-
 }
