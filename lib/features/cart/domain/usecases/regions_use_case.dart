55daed2d11b0a11da21984770e20fd15ecf93f56
qassem abied <qassemab1@gmail.com>
2026-04-05 18:08:53 +0200

    style: format code in regions_use_case.dart for consistency

--- lib/features/cart/domain/usecases/regions_use_case.dart
+++ lib/features/cart/domain/usecases/regions_use_case.dart
@@ -3,16 +3,12 @@ import 'package:my_store/core/network/use_case.dart';
 
 import '../repository.dart';
 
-class RegionsUseCase extends UseCase<String,NoParams>{
+class RegionsUseCase extends UseCase<String, NoParams> {
   final CartRepository _cartRepository;
   RegionsUseCase(this._cartRepository);
 
   @override
-  Future<ApiResult<String>> call(NoParams params)async {
-   return await _cartRepository.getRegions();
-
-
-  
+  Future<ApiResult<String>> call(NoParams params) async {
+    return await _cartRepository.getRegions();
   }
-
 }
