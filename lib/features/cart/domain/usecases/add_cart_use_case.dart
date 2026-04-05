7a1ed58eeb8e73a7b151c7a0af4c99bc9f289273
qassem abied <qassemab1@gmail.com>
2026-04-05 18:06:32 +0200

    update AddCartUseCase to import AddCartRequest from params entity and refine code formatting

--- lib/features/cart/domain/usecases/add_cart_use_case.dart
+++ lib/features/cart/domain/usecases/add_cart_use_case.dart
@@ -1,19 +1,14 @@
-
 import 'package:my_store/core/network/api_result.dart';
-import 'package:my_store/features/cart/domain/entities/add_cart_request.dart';
-
 import '../../../../core/network/use_case.dart';
+import '../entities/params.dart';
 import '../repository.dart';
 
-class AddCartUseCase  extends UseCase<void, AddCartRequest>{
+class AddCartUseCase extends UseCase<void, AddCartRequest> {
   final CartRepository _cartRepository;
   AddCartUseCase(this._cartRepository);
 
   @override
-  Future<ApiResult<void>> call(AddCartRequest params)async {
+  Future<ApiResult<void>> call(AddCartRequest params) async {
     return await _cartRepository.addToCart(params);
-
   }
-
-
 }
