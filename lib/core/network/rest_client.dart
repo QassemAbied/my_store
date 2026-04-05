7e8fa014b22396e59b154ed2d0b8c7264a6f5e70
qassem abied <qassemab1@gmail.com>
2026-04-05 18:08:21 +0200

    add deleteCartItem and updateCartItem methods to RestClient for cart item management

--- lib/core/network/rest_client.dart
+++ lib/core/network/rest_client.dart
@@ -1,60 +1,62 @@
-import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:my_store/core/network/api_contstants.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 import '../../features/cart/data/models/cart_item_model.dart';
-import '../../features/cart/data/models/regions_model.dart';
 import '../../features/home/data/models/product_models.dart';
 import '../../features/products/data/models/product_details_model.dart';
 part 'rest_client.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
-abstract class RestClient  {
-factory RestClient(Dio dio,{String baseUrl})= _RestClient;
-@GET(ApiConstants.productUrl)
+abstract class RestClient {
+  factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
+  @GET(ApiConstants.productUrl)
   Future<ProductResponseModel> getProduct(
     @Query("fields") String fields,
     @Query("limit") int limit,
     @Query("offset") int offset,
-    );
+  );
 
-@GET("${ApiConstants.productUrl}/{id}")
-Future<ProductDetailsModel> getProductDetails(
+  @GET("${ApiConstants.productUrl}/{id}")
+  Future<ProductDetailsModel> getProductDetails(
     @Path("id") String id,
     @Query("fields") String fields,
-    );
+  );
 
-// 🟢 Get Collections
-@GET(ApiConstants.collections)
-Future<void> getCollections(
+  @GET(ApiConstants.collections)
+  Future<void> getCollections(
     @Query("limit") int limit,
     @Query("offset") int offset,
-    );
+  );
 
-// 🟢 Get Categories
-@GET(ApiConstants.categories)
-Future<void> getCategories();
+  @GET(ApiConstants.categories)
+  Future<void> getCategories();
 
-// 🟢 Create Cart
-@POST(ApiConstants.carts)
-Future<String> createCart(
-    @Body() Map<String, dynamic> body,
-    );
+  @POST(ApiConstants.carts)
+  Future<String> createCart(@Body() Map<String, dynamic> body);
 
   @POST("${ApiConstants.carts}/{id}/line-items")
   Future<void> addCart(
-      @Path("id") String id,
-      @Body() Map<String, dynamic> body,
-      );
-
-// 🟢 Get Cart
-@GET("${ApiConstants.carts}/{id}")
-Future<CartResponseModel> getCart(
     @Path("id") String id,
-    );
+    @Body() Map<String, dynamic> body,
+  );
 
-// 🟢 Get Regions
-@GET(ApiConstants.regions)
-Future<String> getRegions();
+  @GET("${ApiConstants.carts}/{id}")
+  Future<CartResponseModel> getCart(@Path("id") String id);
+
+  @GET(ApiConstants.regions)
+  Future<String> getRegions();
+
+  @DELETE("${ApiConstants.carts}/{id}/line-items/{lineId}")
+  Future<void> deleteCartItem(
+    @Path("id") String cartId,
+    @Path("lineId") String lineId,
+  );
+
+  @POST("${ApiConstants.carts}/{id}/line-items/{lineId}")
+  Future<void> updateCartItem(
+    @Path("id") String cartId,
+    @Path("lineId") String lineId,
+    @Body() Map<String, dynamic> body,
+  );
 }
