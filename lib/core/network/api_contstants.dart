827e4cebad86ec53ac1d843062f50a0905308d63
qassem abied <qassemab1@gmail.com>
2026-04-05 09:44:45 +0200

    add collections, categories, carts, and regions endpoints to ApiConstants

--- lib/core/network/api_contstants.dart
+++ lib/core/network/api_contstants.dart
@@ -7,4 +7,9 @@ class ApiConstants {
   static const String publishable="x-publishable-api-key";
   static const String apiKey="pk_ecbf59591d859787be74acff3df180060ed990228d9dba0e95bfdc25182a23e6";
   static const String productUrl= "/store/products";
+
+  static const String collections = "/store/collections";
+  static const String categories = "/store/product-categories";
+  static const String carts = "/store/carts";
+  static const String regions = "/store/regions";
 }
