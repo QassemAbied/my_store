a7bebd2b1924eece031bcabf4f8aca170793bc24
qassem abied <qassemab1@gmail.com>
2026-04-05 18:07:21 +0200

    update CartRemoteDataSource and its implementation to include deleteCartItem and updateCartItem methods using specific params entities

--- lib/features/cart/data/data_source/cart_remote_data_source.dart
+++ lib/features/cart/data/data_source/cart_remote_data_source.dart
@@ -1,12 +1,11 @@
 import 'package:my_store/features/cart/data/models/cart_item_model.dart';
-import 'package:my_store/features/cart/domain/entities/add_cart_request.dart';
-
-import '../models/regions_model.dart';
+import 'package:my_store/features/cart/domain/entities/params.dart';
 
 abstract class CartRemoteDataSource {
   Future<String> getRegions();
   Future<String> createCart(Map<String, dynamic> body);
-
   Future<CartResponseModel> getCartItems(String id);
   Future<void> addToCart(AddCartRequest request);
+  Future<void> deleteCartItem(DeleteCartParams params);
+  Future<void> updateCartItem(UpdateCartParams params);
 }
--- lib/features/cart/data/data_source/cart_remote_data_source_impl.dart
+++ lib/features/cart/data/data_source/cart_remote_data_source_impl.dart
@@ -1,14 +1,12 @@
 import 'package:my_store/core/network/rest_client.dart';
 import 'package:my_store/features/cart/data/models/cart_item_model.dart';
-import 'package:my_store/features/cart/domain/entities/add_cart_request.dart';
-import '../models/regions_model.dart';
+import 'package:my_store/features/cart/domain/entities/params.dart';
 import 'cart_remote_data_source.dart';
 
-class CartRemoteDataSourceImpl  implements CartRemoteDataSource {
+class CartRemoteDataSourceImpl implements CartRemoteDataSource {
   final RestClient _restClient;
 
   CartRemoteDataSourceImpl(this._restClient);
-
 
   @override
   Future<String> getRegions() async {
@@ -21,13 +19,26 @@ class CartRemoteDataSourceImpl  implements CartRemoteDataSource {
   }
 
   @override
-  Future<CartResponseModel> getCartItems(String id)async {
+  Future<CartResponseModel> getCartItems(String id) async {
     return await _restClient.getCart(id);
-
   }
 
   @override
-  Future<void> addToCart(AddCartRequest request)async {
+  Future<void> addToCart(AddCartRequest request) async {
     return await _restClient.addCart(request.cartId, request.body);
+  }
+
+  @override
+  Future<void> deleteCartItem(DeleteCartParams params) async {
+    return await _restClient.deleteCartItem(params.cartId, params.lineId);
+  }
+
+  @override
+  Future<void> updateCartItem(UpdateCartParams params) async {
+    return await _restClient.updateCartItem(
+      params.cartId,
+      params.lineId,
+      params.body,
+    );
   }
 }
