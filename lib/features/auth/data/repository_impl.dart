1c5ef080ff2bcbc867f44eac0afd86c77a2c9fc2
qassem abied <qassemab1@gmail.com>
2026-04-06 17:03:54 +0200

    update AuthRepository and AuthRepositoryImpl to include register method supporting RegisterRequest and CustomerEntity

--- lib/features/auth/data/repository_impl.dart
+++ lib/features/auth/data/repository_impl.dart
@@ -1,9 +1,11 @@
 import 'package:my_store/core/error/failures.dart';
+import 'package:my_store/features/auth/domain/entities/customer_entity.dart';
 
 import '../../../core/network/api_result.dart';
 import '../domain/entities/auth_entity.dart';
 import '../domain/entities/requests.dart';
 import '../domain/mapper/auth_mapper.dart';
+import '../domain/mapper/customer_mapper.dart';
 import '../domain/repository.dart';
 import 'data_source/auth_remote_data_source.dart';
 
@@ -17,6 +19,17 @@ class AuthRepositoryImpl implements AuthRepository {
   async {
     try {
       final result = await remoteDataSource.registerAuth(request);
+
+      return ApiResult.success(result.toEntity());
+    } catch (e) {
+      return ApiResult.failure(ServerFailure());
+    }
+  }
+
+  @override
+  Future<ApiResult<CustomerEntity>> register(RegisterRequest request)async {
+    try {
+      final result = await remoteDataSource.register(request);
 
       return ApiResult.success(result.toEntity());
     } catch (e) {
--- lib/features/auth/domain/repository.dart
+++ lib/features/auth/domain/repository.dart
@@ -1,9 +1,12 @@
 import 'package:my_store/core/network/api_result.dart';
 import 'package:my_store/features/auth/domain/entities/auth_entity.dart';
 
+import 'entities/customer_entity.dart';
 import 'entities/requests.dart';
 
 abstract class AuthRepository{
   Future<ApiResult<AuthEntity>> registerAuth( LoginRequest request);
+  Future<ApiResult<CustomerEntity>> register( RegisterRequest request);
+
 
 }
