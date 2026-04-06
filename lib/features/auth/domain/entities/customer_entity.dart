63a47336f6a0802e9ead940f685f4efee6ec1ec0
qassem abied <qassemab1@gmail.com>
2026-04-06 16:49:15 +0200

    add CustomerEntity for authentication domain entities

---
+++ lib/features/auth/domain/entities/customer_entity.dart
@@ -0,0 +1,15 @@
+class CustomerEntity{
+  final String email;
+
+  final String firstName;
+
+  final String phone;
+  final String country;
+  CustomerEntity({
+    required this.email,
+    required this.firstName,
+    required this.phone, required this.country,
+  });
+
+
+}
