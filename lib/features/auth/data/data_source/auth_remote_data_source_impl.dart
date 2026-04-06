09f2524b751144bf248e0ace3bb169c1637a841e
qassem abied <qassemab1@gmail.com>
2026-04-06 16:46:28 +0200

    add AuthRemoteDataSource and implementation for authentication requests

---
+++ lib/features/auth/data/data_source/auth_remote_data_source_impl.dart
@@ -0,0 +1,19 @@
+import 'package:my_store/features/auth/data/data_source/auth_remote_data_source.dart';
+import 'package:my_store/features/auth/data/models/auth_model.dart';
+import 'package:my_store/features/auth/domain/entities/requests.dart';
+
+import '../../../../core/network/rest_client.dart';
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{
+  final RestClient _restClient;
+
+  AuthRemoteDataSourceImpl(this._restClient);
+
+  @override
+  Future<AuthResponseModel> registerAuth(LoginRequest request)async {
+    return await _restClient.registerAuth({
+      "email": request.email,
+      "password": request.password,
+    });
+  }
+}
