86f241aa8bcc2018bac7345660e4b7011028b2cb
qassem abied <qassemab1@gmail.com>
2026-04-07 23:52:44 +0200

    add CreateAddressParams to cart domain entities

--- lib/features/cart/domain/entities/params.dart
+++ lib/features/cart/domain/entities/params.dart
@@ -26,3 +26,24 @@ class AddShippingOptionParams {
   final Map<String, dynamic> body;
   AddShippingOptionParams(this.cartId, this.body);
 }
+
+
+class CreateAddressParams {
+  final String firstName;
+  final String lastName;
+  final String address1;
+  final String? address2;
+  final String city;
+  final String countryCode;
+  final String? phone;
+
+  CreateAddressParams({
+    required this.firstName,
+    required this.lastName,
+    required this.address1,
+    this.address2,
+    required this.city,
+    required this.countryCode,
+    this.phone,
+  });
+}
