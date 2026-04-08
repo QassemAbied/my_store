4100706b144624f6d0e559b7afc261884ac5fb7f
qassem abied <qassemab1@gmail.com>
2026-04-08 21:59:09 +0200

    update CartEntity with additional fields and add AddressEntity in cart domain entities

--- lib/features/cart/domain/entities/cart_item.dart
+++ lib/features/cart/domain/entities/cart_item.dart
@@ -1,25 +1,35 @@
-
-
-
 class CartResponseEntity {
   final CartEntity cart;
 
   CartResponseEntity({required this.cart});
-
 }
-
 
 class CartEntity {
   final String id;
   final String regionId;
   final int total;
+  final int? subtotal;
+  final int? shippingTotal;
+
+  final String? currencyCode;
+  final String? email;
+
   final List<CartItemEntity> items;
+
+  final AddressEntity? shippingAddress;
+  final AddressEntity? billingAddress;
 
   CartEntity({
     required this.id,
     required this.regionId,
     required this.total,
     required this.items,
+    this.subtotal,
+    this.shippingTotal,
+    this.currencyCode,
+    this.email,
+    this.shippingAddress,
+    this.billingAddress,
   });
 }
 
@@ -34,6 +44,23 @@ class CartItemEntity {
     required this.id,
     required this.title,
     required this.quantity,
-    required this.price, required this.thumbnail,
+    required this.price,
+    required this.thumbnail,
+  });
+}
+
+class AddressEntity {
+  final String? firstName;
+  final String? lastName;
+  final String? address1;
+  final String? city;
+  final String? countryCode;
+
+  AddressEntity({
+    this.firstName,
+    this.lastName,
+    this.address1,
+    this.city,
+    this.countryCode,
   });
 }
