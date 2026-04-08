92b1d663df8e501e06bd7fd0db1edfaebd95bdcb
qassem abied <qassemab1@gmail.com>
2026-04-08 21:58:44 +0200

    add ShippingAddressCartRequest and ShippingAddressRequest to cart domain entities

--- lib/features/cart/domain/entities/params.dart
+++ lib/features/cart/domain/entities/params.dart
@@ -47,3 +47,29 @@ class CreateAddressParams {
     this.phone,
   });
 }
+class ShippingAddressCartRequest {
+  final String cartId;
+  final ShippingAddressRequest body;
+
+  ShippingAddressCartRequest({
+    required this.cartId,
+    required this.body,
+  });
+}
+class ShippingAddressRequest {
+  final String firstName;
+  final String lastName;
+  final String address1;
+  final String city;
+  final String countryCode;
+
+  ShippingAddressRequest({
+    required this.firstName,
+    required this.lastName,
+    required this.address1,
+    required this.city,
+    required this.countryCode,
+  });
+
+
+}
