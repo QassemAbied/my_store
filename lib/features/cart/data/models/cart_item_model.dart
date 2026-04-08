434f01d9f62fe520bf87b06d89dd424b16264033
qassem abied <qassemab1@gmail.com>
2026-04-08 21:58:32 +0200

    update CartModel and add AddressModel to handle extended cart and address data in cart feature

--- lib/features/cart/data/models/cart_item_model.dart
+++ lib/features/cart/data/models/cart_item_model.dart
@@ -2,7 +2,6 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cart_item_model.g.dart';
 
-
 @JsonSerializable()
 class CartResponseModel {
   final CartModel cart;
@@ -22,15 +21,38 @@ class CartModel {
   @JsonKey(name: 'region_id')
   final String regionId;
 
+  @JsonKey(name: 'currency_code')
+  final String? currencyCode;
+
+  final String? email;
+
   final int total;
 
+  @JsonKey(name: 'subtotal')
+  final int? subtotal;
+
+  @JsonKey(name: 'shipping_total')
+  final int? shippingTotal;
+
   final List<CartItemModel> items;
+
+  @JsonKey(name: 'shipping_address')
+  final AddressModel? shippingAddress;
+
+  @JsonKey(name: 'billing_address')
+  final AddressModel? billingAddress;
 
   CartModel({
     required this.id,
     required this.regionId,
     required this.total,
     required this.items,
+    this.currencyCode,
+    this.email,
+    this.subtotal,
+    this.shippingTotal,
+    this.shippingAddress,
+    this.billingAddress,
   });
 
   factory CartModel.fromJson(Map<String, dynamic> json) =>
@@ -47,7 +69,9 @@ class CartItemModel {
   final String title;
 
   final int quantity;
+
   final String thumbnail;
+
   @JsonKey(name: 'unit_price')
   final int price;
 
@@ -55,11 +79,42 @@ class CartItemModel {
     required this.id,
     required this.title,
     required this.quantity,
-    required this.price, required this.thumbnail,
+    required this.price,
+    required this.thumbnail,
   });
 
   factory CartItemModel.fromJson(Map<String, dynamic> json) =>
       _$CartItemModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$CartItemModelToJson(this);
+}
+
+@JsonSerializable()
+class AddressModel {
+  @JsonKey(name: 'first_name')
+  final String? firstName;
+
+  @JsonKey(name: 'last_name')
+  final String? lastName;
+
+  @JsonKey(name: 'address_1')
+  final String? address1;
+
+  final String? city;
+
+  @JsonKey(name: 'country_code')
+  final String? countryCode;
+
+  AddressModel({
+    this.firstName,
+    this.lastName,
+    this.address1,
+    this.city,
+    this.countryCode,
+  });
+
+  factory AddressModel.fromJson(Map<String, dynamic> json) =>
+      _$AddressModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AddressModelToJson(this);
 }
--- lib/features/cart/data/models/cart_item_model.g.dart
+++ lib/features/cart/data/models/cart_item_model.g.dart
@@ -21,13 +21,29 @@ CartModel _$CartModelFromJson(Map<String, dynamic> json) => CartModel(
   items: (json['items'] as List<dynamic>)
       .map((e) => CartItemModel.fromJson(e as Map<String, dynamic>))
       .toList(),
+  currencyCode: json['currency_code'] as String?,
+  email: json['email'] as String?,
+  subtotal: (json['subtotal'] as num?)?.toInt(),
+  shippingTotal: (json['shipping_total'] as num?)?.toInt(),
+  shippingAddress: json['shipping_address'] == null
+      ? null
+      : AddressModel.fromJson(json['shipping_address'] as Map<String, dynamic>),
+  billingAddress: json['billing_address'] == null
+      ? null
+      : AddressModel.fromJson(json['billing_address'] as Map<String, dynamic>),
 );
 
 Map<String, dynamic> _$CartModelToJson(CartModel instance) => <String, dynamic>{
   'id': instance.id,
   'region_id': instance.regionId,
+  'currency_code': instance.currencyCode,
+  'email': instance.email,
   'total': instance.total,
+  'subtotal': instance.subtotal,
+  'shipping_total': instance.shippingTotal,
   'items': instance.items,
+  'shipping_address': instance.shippingAddress,
+  'billing_address': instance.billingAddress,
 };
 
 CartItemModel _$CartItemModelFromJson(Map<String, dynamic> json) =>
@@ -46,4 +62,21 @@ Map<String, dynamic> _$CartItemModelToJson(CartItemModel instance) =>
       'quantity': instance.quantity,
       'thumbnail': instance.thumbnail,
       'unit_price': instance.price,
+    };
+
+AddressModel _$AddressModelFromJson(Map<String, dynamic> json) => AddressModel(
+  firstName: json['first_name'] as String?,
+  lastName: json['last_name'] as String?,
+  address1: json['address_1'] as String?,
+  city: json['city'] as String?,
+  countryCode: json['country_code'] as String?,
+);
+
+Map<String, dynamic> _$AddressModelToJson(AddressModel instance) =>
+    <String, dynamic>{
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+      'address_1': instance.address1,
+      'city': instance.city,
+      'country_code': instance.countryCode,
     };
