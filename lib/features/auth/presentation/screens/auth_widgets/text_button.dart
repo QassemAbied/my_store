e4243590022dd0f143ba21d9573fa952f2f5a9b5
qassem abied <qassemab1@gmail.com>
2026-04-11 02:13:57 +0200

    add PrimaryButton to auth_widgets in auth feature

---
+++ lib/features/auth/presentation/screens/auth_widgets/text_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: TextButton(
+        onPressed: onPressed,
+        child:  Text(text),
+      ),
+    );
+  }
+}
+
+//"Don't have an account? Register"
