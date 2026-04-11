ee4de7ec11fcbfd1561e22723342b221ccf39a54
qassem abied <qassemab1@gmail.com>
2026-04-12 01:56:36 +0200

    add CreateUserForm widget to handle user profile creation in auth feature

---
+++ lib/features/auth/presentation/screens/create_user_profile/widgets/create_user_form.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_store/features/auth/domain/entities/requests.dart';
+import '../../../../../../core/common_widgets/custom_primary_button.dart';
+import '../../../../../../core/common_widgets/custom_text_form_field.dart';
+import '../../../../../../core/theme/color_extension.dart';
+import '../../../../../../core/utils/spacing.dart';
+import '../../../cubit/auth_cubit.dart';
+import 'create_profile_listener.dart';
+
+class CreateUserForm extends StatefulWidget {
+  const CreateUserForm({super.key});
+
+  @override
+  State<CreateUserForm> createState() => _CreateUserFormState();
+}
+
+class _CreateUserFormState extends State<CreateUserForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final countryController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return CreateProfileListener(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            CustomTextFormField(
+              hintText: "Email",
+              textInputType: TextInputType.emailAddress,
+              prefixIcon: Icon(Icons.email,color: context.disabled,),
+              controller: emailController,
+              validator: (value) {
+                if (value?.isEmpty ?? false) {
+                  return "Enter email";
+                }
+                return null;
+              },
+            ),
+            verticalSpace(20),
+            CustomTextFormField(
+              hintText: "First Name",
+              textInputType: TextInputType.name,
+              prefixIcon: Icon(Icons.drive_file_rename_outline,color: context.disabled,),
+              controller: firstNameController,
+              validator: (value) {
+                if (value?.isEmpty ?? false ) {
+                  return "Enter first name";
+                }
+                return null;
+              },
+            ),
+            verticalSpace(20),
+            CustomTextFormField(
+              hintText: "Last Name",
+              textInputType: TextInputType.name,
+              prefixIcon: Icon(Icons.drive_file_rename_outline_rounded,color: context.disabled,),
+              controller: lastNameController,
+              validator: (value) {
+                if (value?.isEmpty ?? false) {
+                  return "Enter Last Name";
+                }
+                return null;
+              },
+            ),
+            verticalSpace(20),
+            CustomTextFormField(
+              hintText: "Phone",
+              textInputType: TextInputType.phone,
+              prefixIcon: Icon(Icons.phone,color: context.disabled,),
+              controller: phoneController,
+              validator: (value) {
+                if (value?.isEmpty ?? false ) {
+                  return "Enter Phone";
+                }
+                return null;
+              },
+            ),
+            verticalSpace(20),
+            CustomTextFormField(
+              hintText: "Country",
+              textInputType: TextInputType.streetAddress,
+              prefixIcon: Icon(Icons.countertops,color: context.disabled,),
+              controller: countryController,
+              validator: (value) {
+                if (value?.isEmpty ?? false ) {
+                  return "Enter Country";
+                }
+                return null;
+              },
+            ),
+            verticalSpace(30),
+            CustomPrimaryButton(
+              text: "Create Profile",
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  context.read<AuthCubit>().createProfile(
+                    RegisterRequest(
+                      email: emailController.text,
+                      firstName: firstNameController.text,
+                      lastName: lastNameController.text,
+                      phone: phoneController.text,
+                      country: countryController.text,
+                    ),
+                  );
+                }
+              },
+            ),
+            verticalSpace(30),
+          ],
+        ),
+      ),
+    );
+  }
+}
