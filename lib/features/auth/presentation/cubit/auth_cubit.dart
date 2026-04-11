236c9f43b12e6b57fb85163d94ff995f08311004
qassem abied <qassemab1@gmail.com>
2026-04-11 02:09:03 +0200

    add logout method to AuthCubit and handle token removal via SharedPrefHelper

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_store/core/services/shared_pref.dart';
 import 'package:my_store/features/auth/domain/entities/requests.dart';
 import 'package:my_store/features/auth/domain/usecases/register_use_case.dart';
 import 'package:my_store/features/auth/presentation/cubit/auth_state.dart';
+import '../../../../core/utils/constants.dart';
 import '../../domain/usecases/login_user_use_case.dart';
 import '../../domain/usecases/register_auth_use_case.dart';
 
@@ -19,7 +21,9 @@ class AuthCubit extends Cubit<AuthState> {
     final result = await _registerAuthUseCase(request);
     result.result.fold(
       (failure) => emit(RegisterAuthError(failure.toString())),
-      (data) => emit(RegisterAuthSuccess(data)),
+      (data) {
+        emit(RegisterAuthSuccess(data));
+      }
     );
   }
 
@@ -38,7 +42,15 @@ class AuthCubit extends Cubit<AuthState> {
     final result = await _loginUserUseCase(request);
     result.result.fold(
           (failure) => emit(LoginError(failure.toString())),
-          (data) => emit(LoginSuccess(data)),
+          (data) {
+           
+            emit(LoginSuccess(data));
+          }
     );
+  }
+
+  Future<void> logout() async {
+    await SharedPrefHelper.removeData(AppConstants.tokenKey);
+    emit(LogoutSuccess());
   }
 }
