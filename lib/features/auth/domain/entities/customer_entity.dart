b05826037bc98b2a12c290937af8f71c7eb3a4a8
qassem abied <qassemab1@gmail.com>
2026-04-12 01:58:43 +0200

    update CustomerEntity to include lastName and make phone and country optional fields

--- lib/features/auth/domain/entities/customer_entity.dart
+++ lib/features/auth/domain/entities/customer_entity.dart
@@ -1,15 +1,15 @@
-class CustomerEntity{
+class CustomerEntity {
   final String email;
-
   final String firstName;
+  final String lastName;
+  final String? phone;
+  final String? country;
 
-  final String phone;
-  final String country;
   CustomerEntity({
     required this.email,
     required this.firstName,
-    required this.phone, required this.country,
+    required this.lastName,
+    this.phone,
+    this.country,
   });
-
-
 }
