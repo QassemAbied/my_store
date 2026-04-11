6d57f36a6812805b870f3342ba69f8db8a23d9c8
qassem abied <qassemab1@gmail.com>
2026-04-12 01:58:36 +0200

    align WelcomeWidget content to the start in auth screens

--- lib/features/auth/presentation/screens/auth_widgets/welcome_widget.dart
+++ lib/features/auth/presentation/screens/auth_widgets/welcome_widget.dart
@@ -11,6 +11,7 @@ class WelcomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         verticalSpace(40),
         Text(
