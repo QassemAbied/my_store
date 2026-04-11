45cc812246a9664decf6f2015900670798faef8e
qassem abied <qassemab1@gmail.com>
2026-04-12 01:59:27 +0200

    add createProfile route to Routers in routing utility

--- lib/core/utils/routing/routers.dart
+++ lib/core/utils/routing/routers.dart
@@ -1,5 +1,6 @@
 class Routers {
   static const String login='/login';
   static const String register='/register';
+  static const String createProfile='/createProfile';
   static const String bottomNav='/bottomNav';
 }
