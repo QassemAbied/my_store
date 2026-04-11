1a887dd3ca7e5d2eeafe54e28bfaf5b0bb07de90
qassem abied <qassemab1@gmail.com>
2026-04-11 02:09:17 +0200

    update login method in AuthRemoteDataSourceImpl to store token in SharedPrefHelper and log it to console

--- lib/features/auth/data/data_source/auth_remote_data_source_impl.dart
+++ lib/features/auth/data/data_source/auth_remote_data_source_impl.dart
@@ -1,9 +1,11 @@
+import 'package:my_store/core/services/shared_pref.dart';
 import 'package:my_store/features/auth/data/data_source/auth_remote_data_source.dart';
 import 'package:my_store/features/auth/data/models/auth_model.dart';
 import 'package:my_store/features/auth/data/models/customer_model.dart';
 import 'package:my_store/features/auth/domain/entities/requests.dart';
 
 import '../../../../core/network/rest_client.dart';
+import '../../../../core/utils/constants.dart';
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{
   final RestClient _restClient;
@@ -31,9 +33,18 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{
 
   @override
   Future<AuthResponseModel> login(LoginRequest request)async {
-    return await _restClient.login({
+    final response = await _restClient.login({
       "email": request.email,
       "password": request.password,
     });
+
+    AppConstants.token== null||AppConstants.token == ''?
+    await SharedPrefHelper.setData(key: AppConstants.tokenKey, value: response.token):null;
+
+
+
+    print("TOKEN: ${response.token}");
+
+    return response;
   }
 }
