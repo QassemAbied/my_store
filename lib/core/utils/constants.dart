479934479589cd3b44bccf59c6544e2156f0cc07
qassem abied <qassemab1@gmail.com>
2026-04-12 01:58:03 +0200

    update `token` type to nullable `String` in `AppConstants`

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -3,5 +3,5 @@ import '../services/shared_pref.dart';
 class AppConstants {
   static const String themeKey='theme';
   static const String tokenKey='TOKEN';
-  static final token=SharedPrefHelper.getString(key: tokenKey);
+  static String? token=SharedPrefHelper.getString(key: tokenKey);
 }
