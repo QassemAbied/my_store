341e915f929c6e936039ebf4a0a367f50a26011f
qassem abied <qassemab1@gmail.com>
2026-04-11 02:09:41 +0200

    add tokenKey constant and token getter to AppConstants using SharedPrefHelper

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,7 @@
+import '../services/shared_pref.dart';
+
 class AppConstants {
   static const String themeKey='theme';
+  static const String tokenKey='TOKEN';
+  static final token=SharedPrefHelper.getString(key: tokenKey);
 }
