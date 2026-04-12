e219514490f667708509a3a7f1d4848d90ecb6a5
qassem abied <qassemab1@gmail.com>
2026-04-12 18:47:49 +0200

    register GetProfileUseCase in injection_container and update AuthCubit factory dependencies

--- lib/features/auth/presentation/screens/login/widget/login_bloc_listener.dart
+++ lib/features/auth/presentation/screens/login/widget/login_bloc_listener.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_store/core/utils/extension.dart';
 import 'package:my_store/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:my_store/features/auth/presentation/cubit/auth_state.dart';
-
 import '../../../../../../core/utils/routing/routers.dart';
 
 class LoginBlocConsumer extends StatelessWidget {
@@ -13,36 +12,41 @@ class LoginBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocListener<AuthCubit, AuthState>(
       listenWhen: (previous, current) =>
-      current is LoginLoading ||
+          current is LoginLoading ||
           current is LoginSuccess ||
-          current is LoginError,
+          current is LoginError ||
+          current is ProfileLoading ||
+          current is ProfileSuccess ||
+          current is ProfileError,
       listener: (context, state) {
         if (state is LoginLoading) {
           showDialog(
             context: context,
             barrierDismissible: false,
-            builder: (_) => const Center(
-              child: CircularProgressIndicator(),
-            ),
+            builder: (_) => const Center(child: CircularProgressIndicator()),
           );
         } else if (state is LoginSuccess) {
+          context.pop();
 
-            context.pop();
+          context.read<AuthCubit>().getProfile();
+        } else if (state is ProfileSuccess) {
+          context.pop();
+
+          context.pushNamedAndRemoveUntil(Routers.bottomNav);
+        } else if (state is ProfileError) {
+          context.pop();
 
           context.pushNamedAndRemoveUntil(Routers.createProfile);
         } else if (state is LoginError) {
-
-            context.pop();
+          context.pop();
 
           showDialog(
             context: context,
-            builder: (_) => AlertDialog(
-              content: Text(state.message),
-            ),
+            builder: (_) => AlertDialog(content: Text(state.message)),
           );
         }
       },
-     child: const SizedBox(),
+      child: const SizedBox(),
     );
   }
 }
