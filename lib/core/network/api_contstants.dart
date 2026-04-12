c07a1bf684058bd19ee237dc32fcedc41491e59c
qassem abied <qassemab1@gmail.com>
2026-04-12 18:46:44 +0200

    add new API endpoints and network constants to ApiConstants

--- lib/core/network/api_contstants.dart
+++ lib/core/network/api_contstants.dart
@@ -6,14 +6,25 @@ class ApiConstants {
   }
   static const String publishable="x-publishable-api-key";
   static const String apiKey="pk_ecbf59591d859787be74acff3df180060ed990228d9dba0e95bfdc25182a23e6";
+  static const String contentType= "Content-Type";
+  static const String applicationJson= "application/json";
+  static const String authorization= "Authorization";
+
+
   static const String productUrl= "/store/products";
 
   static const String collections = "/store/collections";
   static const String categories = "/store/product-categories";
   static const String carts = "/store/carts";
+  static const String addCarts = "/store/carts/{id}/line-items";
+  static const String updateCartsItem = "/store/carts/{id}/line-items/{lineId}";
   static const String address = "/store/custom/addresses";
+  static const String shippingOptions = "/store/shipping-options";
+  static const String addShippingOptions = "/store/carts/{id}/shipping-methods";
+  static const String paymentProvider = "/store/payment-providers";
   static const String regions = "/store/regions";
   static const String authRegister = "/auth/customer/emailpass/register";
   static const String profileRegister = "/store/register";
   static const String login = "/auth/customer/emailpass";
+  static const String profile = "/store/custom/me";
 }
