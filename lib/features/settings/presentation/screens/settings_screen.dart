320fd74ee322252f66ea7c16d60eb9ce3571abb5
qassem abied <qassemab1@gmail.com>
2026-04-12 18:48:29 +0200

    add theme toggle functionality and update AppBar styling in SettingsScreen

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_store/core/utils/extension.dart';
+import 'package:my_store/core/theme/theme_controller/theme_cubit.dart';
+
+import '../../../../core/theme/color_extension.dart';
+import '../../../../core/theme/theme_controller/theme_state.dart';
+import '../../../../core/utils/enum.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,9 +17,28 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
+   // final themeIsDark= Theme.of(context).brightness==Brightness.dark;
     return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      body: const Center(child: Text('Settings Screen')),
+      appBar: AppBar(
+        title: const Text('Settings'),
+        backgroundColor: context.primaryColor,
+      ),
+      body: Center(
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, state) {
+            final themeIsDark = Theme.of(context).brightness == Brightness.dark;
+
+            return ElevatedButton(
+              onPressed: () {
+                context.read<ThemeCubit>().toggleTheme(
+                  themeIsDark ? ThemeEnum.light : ThemeEnum.dark,
+                );
+              },
+              child: const Text('Toggle Theme'),
+            );
+          },
+        ),
+      ),
     );
   }
 }
