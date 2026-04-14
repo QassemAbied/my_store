dba07f99f241a021dee7a70a72358ffb67bb1eb0
qassem abied <qassemab1@gmail.com>
2026-04-14 02:08:38 +0200

    add reginId to ProductParams entity

--- lib/features/home/domain/entities/product_param.dart
+++ lib/features/home/domain/entities/product_param.dart
@@ -2,10 +2,12 @@ class ProductParams {
   final int limit;
   final int offset;
   final String fields;
+  final String reginId;
 
   const ProductParams({
     required this.limit,
     required this.offset,
     required this.fields,
+    required this.reginId,
   });
 }
