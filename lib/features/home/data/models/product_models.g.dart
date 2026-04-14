5603ed4b1ed04e3daef589f1ad534e43c80fdb25
qassem abied <qassemab1@gmail.com>
2026-04-14 02:08:29 +0200

    add variants to ProductItemModels and implement VariantModel and CalculatedPriceModel serialization logic

--- lib/features/home/data/models/product_models.g.dart
+++ lib/features/home/data/models/product_models.g.dart
@@ -33,6 +33,9 @@ ProductItemModels _$ProductItemModelsFromJson(Map<String, dynamic> json) =>
       json['subtitle'] as String?,
       json['description'] as String?,
       json['thumbnail'] as String?,
+      (json['variants'] as List<dynamic>?)
+          ?.map((e) => VariantModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$ProductItemModelsToJson(ProductItemModels instance) =>
@@ -42,4 +45,30 @@ Map<String, dynamic> _$ProductItemModelsToJson(ProductItemModels instance) =>
       'subtitle': instance.subtitle,
       'description': instance.description,
       'thumbnail': instance.thumbnail,
+      'variants': instance.variants,
     };
+
+VariantModel _$VariantModelFromJson(Map<String, dynamic> json) => VariantModel(
+  json['id'] as String?,
+  json['title'] as String?,
+  json['calculated_price'] == null
+      ? null
+      : CalculatedPriceModel.fromJson(
+          json['calculated_price'] as Map<String, dynamic>,
+        ),
+);
+
+Map<String, dynamic> _$VariantModelToJson(VariantModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'calculated_price': instance.calculatedPrice,
+    };
+
+CalculatedPriceModel _$CalculatedPriceModelFromJson(
+  Map<String, dynamic> json,
+) => CalculatedPriceModel((json['calculated_amount'] as num?)?.toInt());
+
+Map<String, dynamic> _$CalculatedPriceModelToJson(
+  CalculatedPriceModel instance,
+) => <String, dynamic>{'calculated_amount': instance.calculated_amount};
