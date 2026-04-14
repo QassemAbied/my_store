96502927eb57e6cf3553821c1f7080d263b45bc5
qassem abied <qassemab1@gmail.com>
2026-04-14 02:08:55 +0200

    update getProducts to include reginId in RemoteDataSourceImpl

--- lib/features/home/data/data_source/remote_data_source_impl.dart
+++ lib/features/home/data/data_source/remote_data_source_impl.dart
@@ -3,11 +3,17 @@ import 'package:my_store/features/home/data/models/product_models.dart';
 import '../../../../core/network/rest_client.dart';
 import '../../domain/entities/product_param.dart';
 
-class RemoteDataSourceImpl implements RemoteDataSource{
+class RemoteDataSourceImpl implements RemoteDataSource {
   final RestClient restClient;
   RemoteDataSourceImpl({required this.restClient});
   @override
-  Future<ProductResponseModel> getProducts(ProductParams params)async {
-  return  restClient.getProduct(params.fields,params.limit,params.offset);
+  Future<ProductResponseModel> getProducts(ProductParams params) async {
+    return restClient.getProduct(
+      params.fields,
+      params.limit,
+      params.offset,
+      params.reginId
+      //params.expand,
+    );
   }
- }
+}
