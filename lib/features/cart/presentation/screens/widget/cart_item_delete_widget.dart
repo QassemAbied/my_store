05ba25518517984be61f69fdcabbd06b49dfb49e
qassem abied <qassemab1@gmail.com>
2026-04-14 02:09:43 +0200

    create CartItemDeleteWidget in cart presentation widgets

---
+++ lib/features/cart/presentation/screens/widget/cart_item_delete_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/theme/app_colors.dart';
+import '../../cubit/cart_cubit.dart';
+
+class CartItemDeleteWidget extends StatelessWidget {
+  final dynamic item;
+  final VoidCallback onDelete;
+
+ const CartItemDeleteWidget({super.key,required this.item, required this.onDelete, });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onDelete,
+
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: AppColors.error.withValues(alpha: 0.1),
+          borderRadius: BorderRadius.circular(12),
+        ),
+
+        child: const Icon(
+          Icons.delete,
+          color: AppColors.error,
+          size: 25,
+        ),
+      ),
+    );
+  }
+}
