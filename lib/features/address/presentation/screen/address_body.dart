3e3b0fd7313a217b20924e9a5bfad1914771b4be
qassem abied <qassemab1@gmail.com>
2026-04-15 18:25:52 +0200

    implement AddressBody with BlocBuilder to handle loading, success, and error states for addresses

---
+++ lib/features/address/presentation/screen/address_body.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_store/features/address/presentation/screen/widgets/address_list_view_widget.dart';
+import 'package:my_store/features/address/presentation/screen/widgets/empty_address_widget.dart';
+import '../controller/address_cubit.dart';
+import '../controller/address_state.dart';
+
+class AddressBody extends StatelessWidget {
+  const AddressBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: BlocBuilder<AddressCubit, AddressState>(
+        builder: (context, state) {
+
+          if (state is AddressLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is AddressSuccess ) {
+            if(state.addresses.addresses.isEmpty){
+              return EmptyAddressWidget();
+            }
+
+            return AddressListViewWidget(addresses: state.addresses,);
+          }
+          if (state is AddressError) {
+            return Center(child: Text(state.message));
+          }
+
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
