cb543a5e50607919cbd7d90a1c387d4ffa2e95c5
qassem abied <qassemab1@gmail.com>
2026-04-15 18:26:27 +0200

    implement AddressListViewWidget to display a reversed list of addresses using AddressListItemWidget

---
+++ lib/features/address/presentation/screen/widgets/address_list_view_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../../../domain/entities/address_entities.dart';
+import 'address_list_item_widget.dart';
+
+class AddressListViewWidget extends StatelessWidget {
+  const AddressListViewWidget({super.key, required this.addresses});
+  final AddressResponseEntity addresses;
+
+  @override
+  Widget build(BuildContext context) {
+    final list = addresses.addresses.reversed.toList();
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      itemCount: list.length,
+
+      itemBuilder: (context, index) {
+        final item = list[index];
+        return AddressListItemWidget(item: item, index: index,);
+      },
+    );
+  }
+}
