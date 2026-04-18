24cf195e182b78875db60b919e4b2d23aab5e151
qassem abied <qassemab1@gmail.com>
2026-04-18 03:25:10 +0200

    add AddShippingOptionsListener widget to handle shipping states and navigation to payment

---
+++ lib/features/shipping/presentation/screen/widget/add_shipping_options_listener.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/utils/extension.dart';
+import '../../../../../core/utils/routing/routers.dart';
+import '../../controller/shipping_cubit.dart';
+import '../../controller/shipping_state.dart';
+
+class AddShippingOptionsListener extends StatelessWidget {
+  const AddShippingOptionsListener({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ShippingCubit, ShippingState>(
+      listenWhen: (previous, current) =>
+      current is AddShippingLoading ||
+          current is AddShippingSuccess ||
+          current is AddShippingError,
+      listener: (context, state) async{
+        if(state is AddShippingLoading){
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (_) => const Center(child: CircularProgressIndicator()),
+          );
+        }
+        else if (state is AddShippingSuccess)  {
+           context.pop();
+          context.pushNamed(Routers.payment);
+        } else if (state is AddShippingError) {
+          context.pop();
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(content: Text(state.message)),
+          );
+        }
+      },
+      child: child,
+    );
+  }
+}
