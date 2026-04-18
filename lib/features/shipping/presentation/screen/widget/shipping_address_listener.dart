1363fb0464aaf33422a3defe51e5546376376299
qassem abied <qassemab1@gmail.com>
2026-04-18 03:26:47 +0200

    create ShippingAddressListener widget to handle shipping address addition states

---
+++ lib/features/shipping/presentation/screen/widget/shipping_address_listener.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_store/features/shipping/presentation/controller/shipping_cubit.dart';
+import '../../../../../core/utils/extension.dart';
+import '../../../../../core/utils/routing/routers.dart';
+import '../../controller/shipping_state.dart';
+
+class ShippingAddressListener extends StatelessWidget {
+  const ShippingAddressListener({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ShippingCubit, ShippingState>(
+      listenWhen: (previous, current) =>
+      current is AddShippingAddressLoading ||
+          current is AddShippingAddressSuccess ||
+          current is AddShippingAddressError,
+      listener: (context, state) async{
+        if (state is AddShippingAddressSuccess)  {
+          context.pushNamed(Routers.shipping);
+        } else if (state is AddShippingAddressError) {
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(content: Text(state.message)),
+          );
+        }
+      },
+      child: child,
+    );
+  }
+}
