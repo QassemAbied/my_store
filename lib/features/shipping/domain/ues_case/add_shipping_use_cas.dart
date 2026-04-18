49ce2c5fa52768ed27dae6b386998d498bf6fdd2
qassem abied <qassemab1@gmail.com>
2026-04-18 03:19:56 +0200

    move AddShippingUseCas to shipping feature and update its repository dependency

--- lib/features/shipping/domain/ues_case/add_shipping_use_cas.dart
+++ lib/features/shipping/domain/ues_case/add_shipping_use_cas.dart
@@ -1,13 +1,13 @@
-import 'package:my_store/features/cart/domain/entities/params.dart';
 import '../../../../core/network/api_result.dart';
 import '../../../../core/network/use_case.dart';
+import '../entities/shipping_request.dart';
 import '../repository.dart';
 
 class AddShippingUseCas extends UseCase<void, AddShippingOptionParams> {
-  final CartRepository _cartRepository;
-  AddShippingUseCas(this._cartRepository);
+  final ShippingRepository _shippingRepository;
+  AddShippingUseCas(this._shippingRepository);
   @override
   Future<ApiResult<void>> call(AddShippingOptionParams params) async {
-    return await _cartRepository.addShippingOptions(params);
+    return await _shippingRepository.addShippingOptions(params);
   }
 }
