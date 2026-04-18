50e0bf2236abf16ea259a1b342769a29c0025213
qassem abied <qassemab1@gmail.com>
2026-04-18 03:24:22 +0200

    remove shipping and address parameter classes from cart domain entities

--- lib/features/cart/domain/entities/params.dart
+++ lib/features/cart/domain/entities/params.dart
@@ -21,55 +21,7 @@ class UpdateCartParams {
   UpdateCartParams(this.cartId, this.lineId,  this.body);
 }
 
-class AddShippingOptionParams {
-  final String cartId;
-  final Map<String, dynamic> body;
-  AddShippingOptionParams(this.cartId, this.body);
-}
 
 
-class CreateAddressParams {
-  final String firstName;
-  final String lastName;
-  final String address1;
-  final String? address2;
-  final String city;
-  final String countryCode;
-  final String? phone;
-
-  CreateAddressParams({
-    required this.firstName,
-    required this.lastName,
-    required this.address1,
-    this.address2,
-    required this.city,
-    required this.countryCode,
-    this.phone,
-  });
-}
-class ShippingAddressCartRequest {
-  final String cartId;
-  final ShippingAddressRequest body;
-
-  ShippingAddressCartRequest({
-    required this.cartId,
-    required this.body,
-  });
-}
-class ShippingAddressRequest {
-  final String firstName;
-  final String lastName;
-  final String address1;
-  final String city;
-  final String countryCode;
-
-  ShippingAddressRequest({
-    required this.firstName,
-    required this.lastName,
-    required this.address1,
-    required this.city,
-    required this.countryCode,
-  });
 
 
-}
