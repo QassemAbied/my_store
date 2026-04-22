fb3021459b43e314b6a65651b94a65992aef3fab
qassem abied <qassemab1@gmail.com>
2026-04-22 06:42:41 +0200

    initialize Stripe, load environment variables, and update BlocProviders in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:my_store/core/services/shared_pref.dart';
 import 'package:my_store/core/theme/app_theme.dart';
 import 'package:my_store/core/theme/theme_controller/theme_state.dart';
@@ -8,6 +10,8 @@ import 'package:my_store/core/utils/routing/routers.dart';
 import 'package:my_store/features/address/presentation/controller/address_cubit.dart';
 import 'package:my_store/features/cart/presentation/cubit/cart_cubit.dart';
 import 'package:my_store/features/home/presentation/cubit/home_cubit.dart';
+import 'package:my_store/features/orders/presentation/controller/order_cubit.dart';
+import 'package:my_store/features/payment/presentation/controller/payment_cubit.dart';
 import 'package:my_store/features/products/presentation/cubit/product_details_cubit.dart';
 import 'package:my_store/features/shipping/presentation/controller/shipping_cubit.dart';
 import 'core/utils/constants.dart';
@@ -20,6 +24,10 @@ import 'injection_container.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await init();
+  await dotenv.load(fileName: ".env");
+  Stripe.publishableKey =dotenv.env['STRIPE_PUBLISHABLE_KEY']!;
+
+  await Stripe.instance.applySettings();
   await SharedPrefHelper.init();
   print(AppConstants.token);
 
@@ -40,8 +48,10 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => sl<CartCubit>()..getCartItems()),
         BlocProvider(create: (context) => sl<HomeCubit>()),
         BlocProvider(create: (context) => sl<AuthCubit>()),
-        BlocProvider(create: (context) => sl<ShippingCubit>()),
-        BlocProvider(create: (context) => sl<AddressCubit>()..getAddresses()),
+        // BlocProvider(create: (context) => sl<OrderCubit>()),
+        // BlocProvider(create: (context) => sl<ShippingCubit>()),
+        //  BlocProvider(create: (context) => sl<PaymentCubit>()),
+        // BlocProvider(create: (context) => sl<AddressCubit>()..getAddresses()),
       ],
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, themeMode) {
@@ -52,10 +62,11 @@ class MyApp extends StatelessWidget {
             themeMode: context.read<ThemeCubit>().themeMode(),
             onGenerateRoute: AppRoute.generateRoute,
             initialRoute:
-            // Routers.payment
-            //
-            AppConstants.token== null||AppConstants.token == ''?
-            Routers.login:Routers.bottomNav,
+                // Routers.success
+                //
+                AppConstants.token == null || AppConstants.token == ''
+                ? Routers.login
+                : Routers.bottomNav,
           );
         },
       ),
