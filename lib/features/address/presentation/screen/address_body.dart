3e126d9930923d4e9de84e641ae116e50e3ff701
qassem abied <qassemab1@gmail.com>
2026-04-22 02:54:23 +0200

    refactor address_body to wrap BlocBuilder with ShippingAddressListener and remove Expanded widget

--- lib/features/address/presentation/screen/address_body.dart
+++ lib/features/address/presentation/screen/address_body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_store/features/address/presentation/screen/widgets/address_list_view_widget.dart';
 import 'package:my_store/features/address/presentation/screen/widgets/empty_address_widget.dart';
+import 'package:my_store/features/shipping/presentation/controller/shipping_cubit.dart';
+import '../../../../injection_container.dart';
 import '../../../shipping/presentation/screen/widget/shipping_address_listener.dart';
 import '../controller/address_cubit.dart';
 import '../controller/address_state.dart';
@@ -11,21 +13,19 @@ class AddressBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return ShippingAddressListener(
       child: BlocBuilder<AddressCubit, AddressState>(
         builder: (context, state) {
-
           if (state is AddressLoading) {
             return const Center(child: CircularProgressIndicator());
           }
 
-          if (state is AddressSuccess ) {
-            if(state.addresses.addresses.isEmpty){
+          if (state is AddressSuccess) {
+            if (state.addresses.addresses.isEmpty) {
               return EmptyAddressWidget();
             }
 
-            return ShippingAddressListener(
-                child: AddressListViewWidget(addresses: state.addresses,));
+            return AddressListViewWidget(addresses: state.addresses,);
           }
           if (state is AddressError) {
             return Center(child: Text(state.message));
