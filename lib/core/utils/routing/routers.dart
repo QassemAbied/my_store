c6f20b7e72e7642800cff2f011c35384be9a4fee
qassem abied <qassemab1@gmail.com>
2026-04-22 06:42:08 +0200

    update Routers class with success and orderDetails constants and fix checkOut value inconsistency

--- lib/core/utils/routing/routers.dart
+++ lib/core/utils/routing/routers.dart
@@ -1,11 +1,12 @@
 class Routers {
-  static const String login='/login';
-  static const String register='/register';
-  static const String createProfile='/createProfile';
-  static const String bottomNav='/bottomNav';
-  static const String cart='/cart';
-  static const String address='/address';
-  static const String shipping='/shipping';
-  static const String payment='/payment';
-  static const String checkOut='/address';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String createProfile = '/createProfile';
+  static const String bottomNav = '/bottomNav';
+  static const String cart = '/cart';
+  static const String address = '/address';
+  static const String shipping = '/shipping';
+  static const String payment = '/payment';
+  static const String success = '/success';
+  static const String orderDetails = '/orderDetails';
 }
