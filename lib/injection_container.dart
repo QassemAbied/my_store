646068851b7c8b260e32c7bb2d64d49a8ff9db5d
qassem abied <qassemab1@gmail.com>
2026-04-22 06:40:33 +0200

    register orders and payment dependencies in injection_container and refactor CartCubit factory dependencies

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -15,7 +15,6 @@ import 'package:my_store/features/cart/data/data_source/cart_remote_data_source.
 import 'package:my_store/features/cart/domain/repository.dart';
 import 'package:my_store/features/cart/domain/usecases/add_cart_use_case.dart';
 import 'package:my_store/features/cart/domain/usecases/cart_item_use_case.dart';
-import 'package:my_store/features/cart/domain/usecases/complete_cart_use_case.dart';
 import 'package:my_store/features/cart/domain/usecases/create_cart_use_case.dart';
 import 'package:my_store/features/cart/domain/usecases/regions_use_case.dart';
 import 'package:my_store/features/cart/domain/usecases/update_cart_use_case.dart';
@@ -25,11 +24,15 @@ import 'package:my_store/features/home/data/data_source/remote_data_source_impl.
 import 'package:my_store/features/home/domain/repositories/home_repositories.dart';
 import 'package:my_store/features/home/domain/usecases/product_usecase.dart';
 import 'package:my_store/features/home/presentation/cubit/home_cubit.dart';
+import 'package:my_store/features/orders/data/data_source/order_remote_data_source.dart';
+import 'package:my_store/features/orders/domain/repository.dart';
+import 'package:my_store/features/orders/presentation/controller/order_cubit.dart';
 import 'package:my_store/features/products/domain/repository.dart';
 import 'package:my_store/features/products/domain/usecases/get_products_details.dart';
 import 'package:my_store/features/products/presentation/cubit/product_details_cubit.dart';
 import 'package:my_store/features/shipping/data/data_source/remote_data_source.dart';
 import 'core/network/rest_client.dart';
+import 'core/services/stripe_service.dart';
 import 'features/address/data/data_source/remote_data_source_impl.dart';
 import 'features/address/data/repository_impl.dart';
 import 'features/auth/data/repository_impl.dart';
@@ -37,6 +40,18 @@ import 'features/auth/domain/usecases/register_use_case.dart';
 import 'features/cart/data/data_source/cart_remote_data_source_impl.dart';
 import 'features/cart/data/repository_impl.dart';
 import 'features/address/domain/ues_case/add_address_use_case.dart';
+import 'features/orders/data/data_source/order_remote_data_source_impl.dart';
+import 'features/orders/data/repository_imp.dart';
+import 'features/orders/domain/use_case/order_review_use_case.dart';
+import 'features/payment/data/data_source/remote_payment_data_source.dart';
+import 'features/payment/data/data_source/remote_payment_data_source_impl.dart';
+import 'features/payment/data/repository_impl.dart';
+import 'features/payment/domain/repository.dart';
+import 'features/payment/domain/use_case/complete_order_use_case.dart';
+import 'features/payment/domain/use_case/payment_collection_use_case.dart';
+import 'features/payment/domain/use_case/payment_provider_use_case.dart';
+import 'features/payment/domain/use_case/payment_session_use_case.dart';
+import 'features/payment/presentation/controller/payment_cubit.dart';
 import 'features/shipping/data/data_source/remote_data_source_impl.dart';
 import 'features/shipping/data/repository_impl.dart';
 import 'features/address/domain/ues_case/delete_address_use_case.dart';
@@ -86,56 +101,69 @@ Future<void> init() async {
   sl.registerLazySingleton<AddCartUseCase>(() => AddCartUseCase(sl()));
   sl.registerLazySingleton<DeleteCartUseCase>(() => DeleteCartUseCase(sl()));
   sl.registerLazySingleton<UpdateCartUseCase>(() => UpdateCartUseCase(sl()));
-  sl.registerLazySingleton<CompleteCartUseCase>(() => CompleteCartUseCase(sl()));
 
-
-  sl.registerFactory(() => CartCubit(sl(), sl(), sl(),
-      sl(),sl(),sl(),sl(), ));
-
-
-
+  sl.registerFactory(() => CartCubit(sl(), sl(), sl(), sl(), sl(), sl()));
 
   sl.registerLazySingleton<AuthRemoteDataSource>(
-        () => AuthRemoteDataSourceImpl(sl()),);
-  sl.registerLazySingleton<AuthRepository>(() =>
-      AuthRepositoryImpl( remoteDataSource: sl(),));
-  sl.registerLazySingleton<RegisterAuthUseCase>(() =>
-      RegisterAuthUseCase(sl()));
-  sl.registerLazySingleton<RegisterUseCase>(() =>
-      RegisterUseCase(sl()));
-  sl.registerLazySingleton<LoginUserUseCase>(() =>
-      LoginUserUseCase(sl()));
-  sl.registerLazySingleton<GetProfileUseCase>(() =>
-      GetProfileUseCase(sl()));
+    () => AuthRemoteDataSourceImpl(sl()),
+  );
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(remoteDataSource: sl()),
+  );
+  sl.registerLazySingleton<RegisterAuthUseCase>(
+    () => RegisterAuthUseCase(sl()),
+  );
+  sl.registerLazySingleton<RegisterUseCase>(() => RegisterUseCase(sl()));
+  sl.registerLazySingleton<LoginUserUseCase>(() => LoginUserUseCase(sl()));
+  sl.registerLazySingleton<GetProfileUseCase>(() => GetProfileUseCase(sl()));
 
-  sl.registerFactory(() => AuthCubit(sl(), sl(),sl(),sl(),));
-
-
+  sl.registerFactory(() => AuthCubit(sl(), sl(), sl(), sl()));
 
   sl.registerLazySingleton<AddressRemoteDataSource>(
-        () => AddressRemoteDataSourceImpl(sl()),);
-  sl.registerLazySingleton<AddressRepository>(() =>
-      AddressRepositoryImpl(  sl(),));
-  sl.registerLazySingleton<GetAddressUseCase>(() =>
-      GetAddressUseCase(sl()));
-  sl.registerLazySingleton<DeleteAddressUseCase>(() =>
-      DeleteAddressUseCase(sl()));
-  sl.registerLazySingleton<AddAddressUseCase>(() =>
-      AddAddressUseCase(sl()));
-  sl.registerFactory(() => AddressCubit(sl(), sl(),sl(),));
-
-
-
+    () => AddressRemoteDataSourceImpl(sl()),
+  );
+  sl.registerLazySingleton<AddressRepository>(
+    () => AddressRepositoryImpl(sl()),
+  );
+  sl.registerLazySingleton<GetAddressUseCase>(() => GetAddressUseCase(sl()));
+  sl.registerLazySingleton<DeleteAddressUseCase>(
+    () => DeleteAddressUseCase(sl()),
+  );
+  sl.registerLazySingleton<AddAddressUseCase>(() => AddAddressUseCase(sl()));
+  sl.registerFactory(() => AddressCubit(sl(), sl(), sl()));
 
   sl.registerLazySingleton<ShippingRemoteDataSource>(
-        () => ShippingRemoteDataSourceImpl(sl()),);
-  sl.registerLazySingleton<ShippingRepository>(() =>
-      ShippingRepositoryImpl(  sl(),));
-  sl.registerLazySingleton<AddShippingAddressUseCase>(() =>
-      AddShippingAddressUseCase(sl()));
-  sl.registerLazySingleton<AddShippingUseCas>(() =>
-      AddShippingUseCas(sl()));
-  sl.registerLazySingleton<ShippingUseCase>(() =>
-      ShippingUseCase(sl()));
-  sl.registerFactory(() => ShippingCubit(sl(), sl(),sl(),));
+    () => ShippingRemoteDataSourceImpl(sl()),
+  );
+  sl.registerLazySingleton<ShippingRepository>(
+    () => ShippingRepositoryImpl(sl()),
+  );
+  sl.registerLazySingleton<AddShippingAddressUseCase>(
+    () => AddShippingAddressUseCase(sl()),
+  );
+  sl.registerLazySingleton<AddShippingUseCas>(() => AddShippingUseCas(sl()));
+  sl.registerLazySingleton<ShippingUseCase>(() => ShippingUseCase(sl()));
+  sl.registerFactory(() => ShippingCubit(sl(), sl(), sl()));
+
+  sl.registerLazySingleton(() => StripeService());
+
+  sl.registerLazySingleton<RemotePaymentDataSource>(
+    () => RemotePaymentDataSourceImpl(sl()),
+  );
+
+  sl.registerLazySingleton<PaymentRepository>(
+    () => PaymentRepositoryImpl(sl()),
+  );
+  sl.registerLazySingleton(() => CreatePaymentCollectionUseCase(sl()));
+  sl.registerLazySingleton(() => PaymentProviderUseCase(sl()));
+  sl.registerLazySingleton(() => CompleteOrderUseCase(sl()));
+  sl.registerLazySingleton(() => PaymentSessionUseCase(sl()));
+  sl.registerFactory(() => PaymentCubit(sl(), sl(), sl(), sl(), sl()));
+
+  sl.registerLazySingleton<OrderRemoteDataSource>(
+    () => OrderRemoteDataSourceImpl(sl()),
+  );
+  sl.registerLazySingleton<OrderRepository>(() => OrderRepositoryImpl(sl()));
+  sl.registerLazySingleton<OrderReviewUseCase>(() => OrderReviewUseCase(sl()));
+  sl.registerFactory(() => OrderCubit(sl()));
 }
