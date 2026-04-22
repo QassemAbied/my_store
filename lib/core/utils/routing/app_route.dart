162897240722d06285fe89045ec8cf9e89255e2d
qassem abied <qassemab1@gmail.com>
2026-04-22 06:38:33 +0200

    update AppRoute to include MultiBlocProvider for AddressScreen and register routes for success and orderDetails screens

--- lib/core/utils/routing/app_route.dart
+++ lib/core/utils/routing/app_route.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_store/core/utils/routing/routers.dart';
-import 'package:my_store/features/address/presentation/screen/address_screen.dart';
-import 'package:my_store/features/auth/presentation/screens/create_user_profile/create_user_profile_screen.dart';
-import 'package:my_store/features/auth/presentation/screens/login/login_screen.dart';
-import 'package:my_store/features/bottom_nav_bar/page/bottom_navigation_bar.dart';
-import 'package:my_store/features/cart/presentation/screens/cart/cart_screen.dart';
-import 'package:my_store/features/cart/presentation/screens/payment/payment_screen.dart';
-import 'package:my_store/features/shipping/presentation/controller/shipping_cubit.dart';
-import 'package:my_store/features/shipping/presentation/screen/shipping_screen.dart';
+
+import '../../../features/address/presentation/controller/address_cubit.dart';
+import '../../../features/address/presentation/screen/address_screen.dart';
+import '../../../features/auth/presentation/screens/create_user_profile/create_user_profile_screen.dart';
+import '../../../features/auth/presentation/screens/login/login_screen.dart';
 import '../../../features/auth/presentation/screens/register/register_screen.dart';
-import '../../../features/cart/domain/entities/params.dart';
-import '../../../features/cart/presentation/cubit/cart_cubit.dart';
+import '../../../features/bottom_nav_bar/page/bottom_navigation_bar.dart';
+import '../../../features/cart/presentation/screens/cart/cart_screen.dart';
+import '../../../features/orders/presentation/controller/order_cubit.dart';
+import '../../../features/orders/presentation/screen/order_review/order_screen.dart';
+import '../../../features/orders/presentation/screen/success/success_screen.dart';
+import '../../../features/payment/presentation/controller/payment_cubit.dart';
+import '../../../features/payment/presentation/screen/payment/payment_screen.dart';
+import '../../../features/shipping/presentation/controller/shipping_cubit.dart';
+import '../../../features/shipping/presentation/screen/shipping_screen.dart';
 import '../../../injection_container.dart';
+
 
 class AppRoute {
   static Route? generateRoute(RouteSettings settings) {
@@ -41,30 +46,59 @@ class AppRoute {
         }
       case Routers.cart:
         {
-          return MaterialPageRoute(
-            builder: (_) => const CartScreen(),
-          );
+          return MaterialPageRoute(builder: (_) => const CartScreen());
         }
       case Routers.address:
         {
           return MaterialPageRoute(
-            builder: (_) => const AddressScreen(),
+            builder: (_) => MultiBlocProvider(
+              providers: [
+                BlocProvider(
+                  create: (context) => sl<AddressCubit>()..getAddresses(),
+                ),
+                BlocProvider(create: (context) => sl<ShippingCubit>()),
+              ],
+              child: Builder(
+                builder: (context) {
+                  return AddressScreen();
+                },
+              ),
+            ),
           );
         }
       case Routers.shipping:
         {
           return MaterialPageRoute(
-            builder: (_) =>
-                BlocProvider(
-                  create: (context) => sl<ShippingCubit>()..getShippingOptions(),
-                  child: ShippingScreen(),
-                ),
+            builder: (_) => BlocProvider(
+              create: (context) => sl<ShippingCubit>()..getShippingOptions(),
+              child: ShippingScreen(),
+            ),
           );
         }
       case Routers.payment:
         {
           return MaterialPageRoute(
-            builder: (_) => const PaymentScreen(),
+            builder: (_) => BlocProvider(
+              create: (context) => sl<PaymentCubit>(),
+              child: PaymentScreen(),
+            ),
+          );
+        }
+      case Routers.success:
+        {
+          final orderId = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (_) => SuccessScreen(orderId: orderId),
+          );
+        }
+      case Routers.orderDetails:
+        {
+          final orderId = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+              create: (context) => sl<OrderCubit>()..loadOrderReview(orderId),
+              child: OrderDetailsScreen(orderId: orderId),
+            ),
           );
         }
     }
