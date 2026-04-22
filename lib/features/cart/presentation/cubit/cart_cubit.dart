d0862c4b2bfb333747ecbd706a4fb5e8cd4d19ea
qassem abied <qassemab1@gmail.com>
2026-04-22 06:38:48 +0200

    remove CompleteCartUseCase from CartCubit and replace checkout with clearCart functionality

--- lib/features/cart/presentation/cubit/cart_cubit.dart
+++ lib/features/cart/presentation/cubit/cart_cubit.dart
@@ -6,7 +6,6 @@ import '../../domain/entities/cart_item.dart';
 import '../../domain/entities/params.dart';
 import '../../domain/usecases/add_cart_use_case.dart';
 import '../../domain/usecases/cart_item_use_case.dart';
-import '../../domain/usecases/complete_cart_use_case.dart';
 import '../../domain/usecases/create_cart_use_case.dart';
 import '../../domain/usecases/delete_cart_use_case.dart';
 import '../../domain/usecases/regions_use_case.dart';
@@ -20,8 +19,6 @@ class CartCubit extends Cubit<CartState> {
   final DeleteCartUseCase deleteCartUseCase;
   final UpdateCartUseCase updateCartUseCase;
 
-   final CompleteCartUseCase completeCartUseCase;
-
   CartCubit(
     this.regionsUseCase,
     this.cartItemUseCase,
@@ -29,15 +26,14 @@ class CartCubit extends Cubit<CartState> {
     this.addCartUseCase,
     this.deleteCartUseCase,
     this.updateCartUseCase,
-      this.completeCartUseCase,
-      ) : super(CartInitial());
+  ) : super(CartInitial());
 
   String? cartId;
   String? regionId;
   final Set<String> selectedItems = {};
-    CartResponseEntity? items;
+  CartResponseEntity? items;
   Future<void> ensureCartId() async {
-    cartId ??=  SharedPrefHelper.getString(key: 'cartId');
+    cartId ??= SharedPrefHelper.getString(key: 'cartId');
 
     if (cartId != null) return;
 
@@ -50,14 +46,9 @@ class CartCubit extends Cubit<CartState> {
 
     final regionId = regionResult.result.getOrElse(() => "");
 
-    await SharedPrefHelper.setData(
-      key: 'region',
-      value: regionId,
-    );
+    await SharedPrefHelper.setData(key: 'region', value: regionId);
 
-    final createResult = await cartUseCase({
-      "region_id": regionId,
-    });
+    final createResult = await cartUseCase({"region_id": regionId});
 
     if (createResult.result.isLeft()) {
       emit(CartItemsError("Create Cart Error"));
@@ -68,11 +59,9 @@ class CartCubit extends Cubit<CartState> {
 
     cartId = newCartId;
 
-    await SharedPrefHelper.setData(
-      key: 'cartId',
-      value: newCartId,
-    );
+    await SharedPrefHelper.setData(key: 'cartId', value: newCartId);
   }
+
   Future<void> getCartItems() async {
     emit(CartItemsLoading());
 
@@ -85,49 +74,42 @@ class CartCubit extends Cubit<CartState> {
 
     final result = await cartItemUseCase(cartId!);
 
-    result.result.fold(
-      (failure) => emit(CartItemsError("Get Cart Error")),
-      (cartData) {
-        items=cartData;
-        selectedItems.clear();
+    result.result.fold((failure) => emit(CartItemsError("Get Cart Error")), (
+      cartData,
+    ) {
+      items = cartData;
+      selectedItems.clear();
 
-        selectedItems.addAll(
-          items!.cart.items.map((e) => e.variantId),
-        );
-        emit(CartItemsSuccess(cartData));}
-    );
+      selectedItems.addAll(items!.cart.items.map((e) => e.variantId));
+      emit(CartItemsSuccess(cartData));
+    });
   }
-  Future<void> addCart({required String variantId, required int quantity, required CartItemEntity cartItem,})
-  async {
+
+  Future<void> addCart({
+    required String variantId,
+    required int quantity,
+    required CartItemEntity cartItem,
+  }) async {
     regionId ??= SharedPrefHelper.getString(key: 'region');
 
     await ensureCartId();
     if (cartId == null) return;
 
     items ??= CartResponseEntity(
-      cart: CartEntity(
-        items: [],
-        total: 0,
-        id: cartId!,
-        regionId: regionId!,
-      ),
+      cart: CartEntity(items: [], total: 0, id: cartId!, regionId: regionId!),
     );
 
-    final index = items!.cart.items.indexWhere(
-          (e) => e.variantId == variantId,
-    );
+    final index = items!.cart.items.indexWhere((e) => e.variantId == variantId);
 
     if (index != -1) {
-
       items!.cart.items[index].quantity += quantity;
     } else {
-
       items!.cart.items.insert(0, cartItem);
     }
 
     items!.cart.total = items!.cart.items.fold<int>(
       0,
-          (sum, item) => sum + (item.price * item.quantity),
+      (sum, item) => sum + (item.price * item.quantity),
     );
 
     emit(CartItemsSuccess(items!));
@@ -135,43 +117,42 @@ class CartCubit extends Cubit<CartState> {
 
     try {
       await addCartUseCase(
-        AddCartRequest(
-          cartId!,
-          {"variant_id": variantId, "quantity": quantity},
-        ),
+        AddCartRequest(cartId!, {
+          "variant_id": variantId,
+          "quantity": quantity,
+        }),
       );
 
       await getCartItems();
-
     } catch (e) {
       if (index != -1) {
         items!.cart.items[index].quantity -= quantity;
       } else {
-        items!.cart.items.removeWhere(
-              (e) => e.variantId == variantId,
-        );
+        items!.cart.items.removeWhere((e) => e.variantId == variantId);
       }
 
       items!.cart.total = items!.cart.items.fold<int>(
         0,
-            (sum, item) => sum + (item.price * item.quantity),
+        (sum, item) => sum + (item.price * item.quantity),
       );
 
       emit(CartItemsSuccess(items!));
     }
   }
-  Future<void> deleteCart({required String lineId, required String variantId,})
-  async {
+
+  Future<void> deleteCart({
+    required String lineId,
+    required String variantId,
+  }) async {
     if (items == null) return;
 
-    final removedItem =
-    items!.cart.items.firstWhere((e) => e.id == lineId);
+    final removedItem = items!.cart.items.firstWhere((e) => e.id == lineId);
 
     items!.cart.items.removeWhere((e) => e.id == lineId);
 
     items!.cart.total = items!.cart.items.fold<int>(
       0,
-          (sum, item) => sum + (item.price * item.quantity),
+      (sum, item) => sum + (item.price * item.quantity),
     );
 
     selectedItems.remove(variantId);
@@ -179,15 +160,13 @@ class CartCubit extends Cubit<CartState> {
     emit(CartItemsSuccess(items!));
 
     try {
-      await deleteCartUseCase(
-        DeleteCartParams(cartId!, lineId),
-      );
+      await deleteCartUseCase(DeleteCartParams(cartId!, lineId));
     } catch (e) {
       items!.cart.items.insert(0, removedItem);
 
       items!.cart.total = items!.cart.items.fold<int>(
         0,
-            (sum, item) => sum + (item.price * item.quantity),
+        (sum, item) => sum + (item.price * item.quantity),
       );
 
       selectedItems.add(variantId);
@@ -195,12 +174,14 @@ class CartCubit extends Cubit<CartState> {
       emit(CartItemsSuccess(items!));
     }
   }
-  Future<void> updateCart({required String lineId, required int quantity,})
-  async {
+
+  Future<void> updateCart({
+    required String lineId,
+    required int quantity,
+  }) async {
     if (items == null) return;
 
-    final item =
-    items!.cart.items.firstWhere((e) => e.id == lineId);
+    final item = items!.cart.items.firstWhere((e) => e.id == lineId);
 
     final oldQuantity = item.quantity;
 
@@ -208,7 +189,7 @@ class CartCubit extends Cubit<CartState> {
 
     items!.cart.total = items!.cart.items.fold<int>(
       0,
-          (sum, item) => sum + (item.price * item.quantity),
+      (sum, item) => sum + (item.price * item.quantity),
     );
 
     emit(CartItemsSuccess(items!));
@@ -222,36 +203,20 @@ class CartCubit extends Cubit<CartState> {
 
       items!.cart.total = items!.cart.items.fold<int>(
         0,
-            (sum, item) => sum + (item.price * item.quantity),
+        (sum, item) => sum + (item.price * item.quantity),
       );
 
       emit(CartItemsSuccess(items!));
     }
   }
 
+  Future<void> clearCart() async {
+    items = null;
+    selectedItems.clear();
+    cartId = null;
 
+    await SharedPrefHelper.removeData('cartId');
 
-  Future<void> checkout() async {
-    emit(CheckoutLoading());
-
-    await ensureCartId();
-
-    if (cartId == null) {
-      emit(CheckoutError("Cart Id is null"));
-      return;
-    }
-
-    final result = await completeCartUseCase(cartId!);
-
-    result.result.fold(
-          (failure) => emit(CheckoutError("Checkout failed")),
-          (_) async {
-        // 🧹 ممكن تمسح الكارت بعد النجاح
-        cartId = null;
-        await SharedPrefHelper.removeData( 'cartId');
-
-        emit(CheckoutSuccess());
-      },
-    );
+    emit(CartInitial());
   }
 }
