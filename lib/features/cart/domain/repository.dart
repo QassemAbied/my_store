6057219fb174e03c4ed2364bcd1dd0848e89a4e5
qassem abied <qassemab1@gmail.com>
2026-04-22 06:41:28 +0200

    move payment provider entities and mappers to payment feature and update cart repository imports

--- lib/features/cart/domain/repository.dart
+++ lib/features/cart/domain/repository.dart
@@ -3,7 +3,7 @@ import 'package:my_store/features/cart/domain/entities/cart_item.dart';
 
 import '../../address/domain/entities/address_entities.dart';
 import 'entities/params.dart';
-import 'entities/payment_provider_entities.dart';
+import '../../payment/domain/entities/payment_provider_entities.dart';
 import '../../shipping/domain/entities/shipping_entites.dart';
 
 abstract class CartRepository {
