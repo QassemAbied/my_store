a1800bf5b282cd74dbc1aa314686837f78f1bc5b
qassem abied <qassemab1@gmail.com>
2026-04-22 06:41:52 +0200

    update Android dependencies and refactor MainActivity to FlutterFragmentActivity
    - Update Android application plugin version to 8.13.2 and Kotlin version to 2.3.10 in `settings.gradle.kts`
    - Update `NormalTheme` to use `Theme.MaterialComponents` in both light and night style resources
    - Change `MainActivity` base class from `FlutterActivity` to `FlutterFragmentActivity`

--- android/app/src/main/kotlin/com/example/my_store/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_store/MainActivity.kt
@@ -2,4 +2,7 @@ package com.example.my_store
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
