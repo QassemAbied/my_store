b1c39fcd098ad23ba7ca10f246b6d005b940506b
qassem abied <qassemab1@gmail.com>
2026-04-25 11:25:41 +0300

    refactor product entities and mappers to core and update HomeRepositories imports

--- lib/features/home/data/repositories_impl/home_repositories_impl.dart
+++ lib/features/home/data/repositories_impl/home_repositories_impl.dart
@@ -1,11 +1,11 @@
 import 'package:my_store/core/error/failures.dart';
 import 'package:my_store/core/network/api_result.dart';
 import 'package:my_store/features/home/data/data_source/remote_data_source.dart';
-import 'package:my_store/features/home/domain/entities/product_entities.dart';
+import 'package:my_store/core/common_models/entities/product_entities.dart';
 import 'package:my_store/features/home/domain/repositories/home_repositories.dart';
 
 import '../../domain/entities/product_param.dart';
-import '../../domain/mappers/product_mapper.dart';
+import '../../../../core/common_models/mapper/product_mapper.dart';
 
 class HomeRepositoriesImpl implements HomeRepositories{
   final RemoteDataSource remoteDataSource;
