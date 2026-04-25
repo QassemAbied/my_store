d95c33f41ac87d6351a706c8ee899def126878ac
qassem abied <qassemab1@gmail.com>
2026-04-25 11:27:17 +0300

    update product_entities import path in ProductUseCase to core/common_models

--- lib/features/home/domain/usecases/product_usecase.dart
+++ lib/features/home/domain/usecases/product_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:my_store/core/network/api_result.dart';
 import 'package:my_store/core/network/use_case.dart';
 
-import '../entities/product_entities.dart';
+import '../../../../core/common_models/entities/product_entities.dart';
 import '../entities/product_param.dart';
 import '../repositories/home_repositories.dart';
 
