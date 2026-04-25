26c328a128003dcf6090956fa5abd9961d623c73
qassem abied <qassemab1@gmail.com>
2026-04-26 02:11:21 +0300

    add ProductsShimmer widget to category presentation widgets

---
+++ lib/features/category/presentation/screens/widgets/product_loading_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import '../../../../../core/theme/color_extension.dart';
+import '../../../../../core/utils/spacing.dart';
+
+class ProductsShimmer extends StatelessWidget {
+  const ProductsShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 6,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        childAspectRatio: 0.75,
+      ),
+      itemBuilder: (_, _) {
+        return Shimmer.fromColors(
+          baseColor: context.border,
+          highlightColor: context.surfaceColor,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 12,
+                        width: double.infinity,
+                        color: Colors.white,
+                      ),
+                      verticalSpace(6),
+                      Container(
+                        height: 12,
+                        width: 80,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
