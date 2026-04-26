1149fcc9689e2e2d3787228b90f9913147c9a41c
qassem abied <qassemab1@gmail.com>
2026-04-27 02:57:56 +0300

    add query field to ProductParams entity

--- lib/features/home/domain/entities/product_param.dart
+++ lib/features/home/domain/entities/product_param.dart
@@ -3,11 +3,12 @@ class ProductParams {
   final int offset;
   final String fields;
   final String reginId;
+  final String? query;
 
   const ProductParams({
     required this.limit,
     required this.offset,
     required this.fields,
-    required this.reginId,
+    required this.reginId, required this.query,
   });
 }
