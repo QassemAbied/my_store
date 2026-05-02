bc6a6deca042c5a2ed632fca7fddbb6eed73e450
qassem abied <qassemab1@gmail.com>
2026-05-02 19:16:13 +0300

    add authIdentityId field to RegisterRequest entity

--- lib/features/auth/domain/entities/requests.dart
+++ lib/features/auth/domain/entities/requests.dart
@@ -8,12 +8,12 @@ class RegisterRequest{
   final String email;
   final String firstName;
   final String lastName;
-
+  final String authIdentityId;
   final String phone;
   final String country;
 
   RegisterRequest({ required this.email,
     required this.firstName, required this.phone,required this.country,
-    required this.lastName}
+    required this.lastName, required this.authIdentityId}
       );
 }
