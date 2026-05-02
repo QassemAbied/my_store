7b46b11baacfcf9cf689ca471355203a9d191723
qassem abied <qassemab1@gmail.com>
2026-05-02 19:15:02 +0300

    add currencyCode, createdAt, total, and name fields to OrderReviewEntity and ShippingAddressEntity

--- lib/features/orders/domain/entitiy/order_review_entities.dart
+++ lib/features/orders/domain/entitiy/order_review_entities.dart
@@ -1,14 +1,21 @@
 class OrderReviewEntity {
   final String id;
   final String status;
+  final String currencyCode;
+  final String createdAt;
   final String? email;
+  final int total;
+
   final List<OrderItemEntity> items;
   final ShippingAddressEntity? address;
 
   OrderReviewEntity({
     required this.id,
     required this.status,
+    required this.currencyCode,
+    required this.createdAt,
     required this.items,
+    required this.total,
     this.email,
     this.address,
   });
@@ -32,10 +39,14 @@ class ShippingAddressEntity {
   final String address;
   final String city;
   final String country;
+  final String? firstName;
+  final String? lastName;
 
   ShippingAddressEntity({
     required this.address,
     required this.city,
     required this.country,
+    this.firstName,
+    this.lastName,
   });
 }
