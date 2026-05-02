0838323a5af78147f6ae9d413438e4c795f40b89
qassem abied <qassemab1@gmail.com>
2026-05-02 19:19:07 +0300

    add LoadingOrderHistoryWidget with shimmer loading effects for the order history list

---
+++ lib/features/orders/presentation/screen/order_history/widgets/loading_order_history_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class OrderShimmerItem extends StatelessWidget {
+  const OrderShimmerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade300,
+      highlightColor: Colors.grey.shade100,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.only(bottom: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            /// header
+            Container(height: 16, width: 100, color: Colors.white),
+
+            const SizedBox(height: 10),
+
+            Container(height: 12, width: 150, color: Colors.white),
+
+            const SizedBox(height: 12),
+
+            /// items preview
+            Row(
+              children: List.generate(
+                3,
+                    (index) => Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  width: 60,
+                  height: 60,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            /// total
+            Container(height: 14, width: 80, color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OrdersShimmerList extends StatelessWidget {
+  const OrdersShimmerList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 6,
+      itemBuilder: (_, __) => const OrderShimmerItem(),
+    );
+  }
+}
