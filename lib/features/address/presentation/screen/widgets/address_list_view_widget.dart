e756c75dc49f4eb6909987eca8231734ee18a212
qassem abied <qassemab1@gmail.com>
2026-05-02 19:07:44 +0300

    add address selection logic and ShippingCubit integration to AddressListViewWidget

--- lib/features/address/presentation/screen/widgets/address_list_view_widget.dart
+++ lib/features/address/presentation/screen/widgets/address_list_view_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../shipping/domain/entities/shipping_request.dart';
+import '../../../../shipping/presentation/controller/shipping_cubit.dart';
 import '../../../domain/entities/address_entities.dart';
+import '../../controller/address_cubit.dart';
 import 'address_list_item_widget.dart';
 
 class AddressListViewWidget extends StatelessWidget {
@@ -9,12 +13,29 @@ class AddressListViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final list = addresses.addresses.reversed.toList();
+    final cubit = context.watch<AddressCubit>();
+
     return ListView.builder(
       itemCount: list.length,
 
       itemBuilder: (context, index) {
         final item = list[index];
-        return AddressListItemWidget(item: item, index: index);
+        final selected = cubit.selectedIndex == index;
+
+        return GestureDetector(
+            onTap: () {
+              cubit.selectAddress(index);
+              context.read<ShippingCubit>().addShippingAddress( body:
+              ShippingAddressRequest(
+              firstName: item.firstName ?? '',
+              lastName: item.lastName ?? '',
+              address1: item.address1 ?? '',
+              city: item.city ?? '',
+              countryCode: item.countryCode ?? '',
+              ),
+);
+            },
+            child: AddressListItemWidget(item: item, index: index, selected: selected,));
       },
     );
   }
