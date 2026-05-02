8f8f5fba73b7cf6aa0cb6b908c76aa8571214e0a
qassem abied <qassemab1@gmail.com>
2026-05-02 19:16:19 +0300

    update `CommonWidget` with new utility methods for formatting dates and mapping statuses, and enhance `leadingIcon` with customizable colors

--- lib/core/common_widgets/widgets.dart
+++ lib/core/common_widgets/widgets.dart
@@ -2,28 +2,70 @@ import 'package:flutter/material.dart';
 
 import '../theme/color_extension.dart';
 
-class CommonWidget{
- static Widget selectedItem(bool selected, BuildContext context ){
+class CommonWidget {
+  static Widget selectedItem(bool selected, BuildContext context) {
     return Icon(
-      selected
-          ? Icons.check_box
-          : Icons.check_box_outline_blank,
-      color: selected
-          ? context.primaryColor
-          : context.disabled,
+      selected ? Icons.check_box : Icons.check_box_outline_blank,
+      color: selected ? context.primaryColor : context.disabled,
     );
   }
 
-  static Widget leadingIcon( BuildContext context ,IconData icon){
-  return  Container(
+  static Widget leadingIcon({
+    required BuildContext context,
+    required IconData icon,
+    Color? iconColor,
+    Color? backgroundIconColor,
+  })
+  {
+    final iconClr = iconColor ?? context.primaryColor;
+    final bgClr =
+        backgroundIconColor ?? context.primaryColor.withValues(alpha: 0.1);
+    return Container(
       padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: context.primaryColor.withValues(alpha: 0.1),
+        color: bgClr,
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Icon(icon, color: context.primaryColor),
+      child: Icon(icon, color: iconClr),
     );
   }
 
+  static String formatDate(String date) {
+    final d = DateTime.parse(date);
+    return "${d.day}/${d.month}/${d.year}";
+  }
 
+  static Widget statusDot(BuildContext context, String status) {
+    Color color;
+
+    switch (status) {
+      case "pending":
+        color = context.warning;
+        break;
+      case "completed":
+        color = context.success;
+        break;
+      default:
+        color = context.error;
+    }
+
+    return Container(
+      width: 10,
+      height: 10,
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+    );
+  }
+
+  static String mapStatus(String status) {
+    switch (status.toLowerCase()) {
+      case "pending":
+        return "Processing";
+      case "completed":
+        return "Delivered";
+      case "canceled":
+        return "Canceled";
+      default:
+        return status;
+    }
+  }
 }
