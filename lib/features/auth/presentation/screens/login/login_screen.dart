a04828e8dd874e0336dc486ef83ae229f854f357
qassem abied <qassemab1@gmail.com>
2026-05-02 19:14:55 +0300

    remove unused import in login_screen.dart

--- lib/features/auth/presentation/screens/login/login_screen.dart
+++ lib/features/auth/presentation/screens/login/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_store/features/auth/presentation/screens/login/login_body.dart';
-
 import '../../../../../injection_container.dart';
 import '../../cubit/auth_cubit.dart';
 
