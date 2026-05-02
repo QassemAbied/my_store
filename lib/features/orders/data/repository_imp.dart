2f70ed83cfcdb51814914352e62c0282b2efb06a
qassem abied <qassemab1@gmail.com>
2026-05-02 19:12:37 +0300

    add getOrderList to OrderRepository and update getOrderReview mapping logic

--- lib/features/orders/data/repository_imp.dart
+++ lib/features/orders/data/repository_imp.dart
@@ -1,6 +1,9 @@
+import 'package:my_store/features/orders/domain/entitiy/order_list_entitiy.dart';
+
 import '../../../core/error/failures.dart';
 import '../../../core/network/api_result.dart';
 import '../domain/entitiy/order_review_entities.dart';
+import '../domain/mapper/order_list_mapper.dart';
 import '../domain/mapper/order_review_mapper.dart';
 import '../domain/repository.dart';
 import 'data_source/order_remote_data_source.dart';
@@ -14,6 +17,18 @@ class OrderRepositoryImpl implements OrderRepository {
   Future<ApiResult<OrderReviewEntity>> getOrderReview(String orderId) async {
     try {
       final res = await _remote.getOrderReview(orderId);
+
+      return ApiResult.success(res.order.toEntity());
+    } catch (failure) {
+      return ApiResult.failure(ServerFailure());
+    }
+  }
+
+  @override
+  Future<ApiResult<OrdersEntity>> getOrderList() async {
+    try {
+      final res = await _remote.getOrderList();
+
       return ApiResult.success(res.toEntity());
     } catch (failure) {
       return ApiResult.failure(ServerFailure());
--- lib/features/orders/domain/repository.dart
+++ lib/features/orders/domain/repository.dart
@@ -1,7 +1,9 @@
 import 'package:my_store/core/network/api_result.dart';
 
+import 'entitiy/order_list_entitiy.dart';
 import 'entitiy/order_review_entities.dart';
 
 abstract class OrderRepository {
   Future<ApiResult<OrderReviewEntity>> getOrderReview(String orderId);
+  Future<ApiResult<OrdersEntity>> getOrderList();
 }
