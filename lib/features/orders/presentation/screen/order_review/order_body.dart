d9679b876ae092f19ce73ea8107a2ba5ef492f3c
qassem abied <qassemab1@gmail.com>
2026-05-02 19:13:16 +0300

    refactor order_body.dart UI to use SliverAppBar and update order review widgets

--- lib/features/orders/presentation/screen/order_review/order_body.dart
+++ lib/features/orders/presentation/screen/order_review/order_body.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_store/features/orders/presentation/screen/order_review/widgets/address_widget.dart';
 import 'package:my_store/features/orders/presentation/screen/order_review/widgets/dummy_payment_widget.dart';
-import 'package:my_store/features/orders/presentation/screen/order_review/widgets/header_order_review_widget.dart';
-import 'package:my_store/features/orders/presentation/screen/order_review/widgets/time_line_order_widget.dart';
+import 'package:my_store/features/orders/presentation/screen/order_review/widgets/hero_header_widget.dart';
+import 'package:my_store/features/orders/presentation/screen/order_review/widgets/total_card_widget.dart';
 import '../../../../../core/common_widgets/custom_order_summery/order_summery_widget.dart';
 import '../../../../../core/utils/spacing.dart';
 import '../../controller/order_cubit.dart';
@@ -25,30 +25,44 @@ class OrderBody extends StatelessWidget {
         }
 
         if (state is OrderReviewSuccess) {
-          final order = state.order;
-          final total = order.items.fold(
-            0,
-            (sum, e) => sum + (e.price * e.quantity),
-          );
+           final order = state.order;
+           return CustomScrollView(
+             slivers: [
+               SliverAppBar(
+                 expandedHeight: 250,
+                 pinned: true,
+                 flexibleSpace: FlexibleSpaceBar(
+                   background: HeroHeaderWidget( order: order,),
+                 ),
+               ),
 
-          return CustomScrollView(
-            slivers: [
-              HeaderOrderReviewWidget(order: order),
-              const TimeLineOrderWidget(),
-              SliverToBoxAdapter(child: verticalSpace(10)),
-              const DummyPaymentWidget(),
-              SliverToBoxAdapter(child: verticalSpace(10)),
-              OrderSummeryWidget(
-                items: order.items,
-                subtotal: total,
-                shippingTotal: 0,
-                total: total,
-              ),
-              SliverToBoxAdapter(child: verticalSpace(10)),
-              AddressWidget(order: order),
-              SliverToBoxAdapter(child: verticalSpace(50)),
-            ],
-          );
+               SliverPadding(
+                 padding:  EdgeInsets.all(
+                     16
+                 ),
+                 sliver: SliverList(
+                   delegate: SliverChildListDelegate([
+                     verticalSpace(20),
+
+                     OrderSummeryWidget(
+                         items: order.items,
+                       item: order,
+                       isShow: false,
+                     ),
+                     verticalSpace(20),
+                     if (order.address != null)
+                       AddressWidget(order: order,),
+                     verticalSpace(16),
+                     DummyPaymentWidget(),
+                     verticalSpace(20),
+                     TotalCardWidget(order: order,),
+
+
+                   ]),
+                 ),
+               ),
+             ],
+           );
         }
 
         return const SizedBox();
