c4cb6f13efca4cc1bbe7be3099c94d660f158b10
qassem abied <qassemab1@gmail.com>
2026-05-02 19:10:30 +0300

    add OrderList states and loadOrderList method to OrderCubit

--- lib/features/orders/presentation/controller/order_cubit.dart
+++ lib/features/orders/presentation/controller/order_cubit.dart
@@ -1,11 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/network/use_case.dart';
+import '../../domain/use_case/order_list_use_case.dart';
 import '../../domain/use_case/order_review_use_case.dart';
 import 'order_state.dart';
 
 class OrderCubit extends Cubit<OrderState> {
   final OrderReviewUseCase _orderReviewUseCase;
-  OrderCubit(this._orderReviewUseCase) : super(OrderInitial());
+  final OrderListUseCase _orderListUseCase;
+
+  OrderCubit(this._orderReviewUseCase,this._orderListUseCase)
+      : super(OrderInitial());
+
+
+
 
   Future<void> loadOrderReview(String orderId) async {
     emit(OrderReviewLoading());
@@ -16,4 +24,15 @@ class OrderCubit extends Cubit<OrderState> {
       (data) => emit(OrderReviewSuccess(data)),
     );
   }
+
+  Future<void> loadOrderList() async {
+    emit(OrderListLoading());
+    final result = await _orderListUseCase(NoParams());
+
+    result.result.fold(
+          (failure) => emit(OrderListError(failure.toString())),
+          (data) => emit(OrderListSuccess(data)),
+    );
+  }
+
 }
--- lib/features/orders/presentation/controller/order_state.dart
+++ lib/features/orders/presentation/controller/order_state.dart
@@ -1,3 +1,4 @@
+import '../../domain/entitiy/order_list_entitiy.dart';
 import '../../domain/entitiy/order_review_entities.dart';
 
 abstract class OrderState {}
@@ -16,4 +17,20 @@ class OrderReviewError extends OrderState {
   final String message;
 
   OrderReviewError(this.message);
+}
+
+
+
+class OrderListLoading extends OrderState {}
+
+class OrderListSuccess extends OrderState {
+  final OrdersEntity order;
+
+  OrderListSuccess(this.order);
+}
+
+class OrderListError extends OrderState {
+  final String message;
+
+  OrderListError(this.message);
 }
