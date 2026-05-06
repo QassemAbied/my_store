02dd5e3aa2d1852d95a06ac6e155cd1ef9603dc3
qassem abied <qassemab1@gmail.com>
2026-05-06 13:40:38 +0300

    remove unused BlocProviders and wrap token print in debug mode check in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -7,14 +8,9 @@ import 'package:my_store/core/theme/app_theme.dart';
 import 'package:my_store/core/theme/theme_controller/theme_state.dart';
 import 'package:my_store/core/utils/routing/app_route.dart';
 import 'package:my_store/core/utils/routing/routers.dart';
-import 'package:my_store/features/address/presentation/controller/address_cubit.dart';
 import 'package:my_store/features/cart/presentation/cubit/cart_cubit.dart';
 import 'package:my_store/features/category/presentation/controller/category_cubit.dart';
 import 'package:my_store/features/home/presentation/cubit/home_cubit.dart';
-import 'package:my_store/features/orders/presentation/controller/order_cubit.dart';
-import 'package:my_store/features/payment/presentation/controller/payment_cubit.dart';
-import 'package:my_store/features/products/presentation/cubit/product_details_cubit.dart';
-import 'package:my_store/features/shipping/presentation/controller/shipping_cubit.dart';
 import 'core/utils/constants.dart';
 import 'features/auth/presentation/cubit/auth_cubit.dart';
 import 'features/bottom_nav_bar/controller/bottom_nav_cubit.dart';
@@ -30,7 +26,9 @@ void main() async {
 
   await Stripe.instance.applySettings();
   await SharedPrefHelper.init();
-  print(AppConstants.token);
+  if (kDebugMode) {
+    print(AppConstants.token);
+  }
 
   runApp(const MyApp());
 }
@@ -43,31 +41,23 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => sl<CartCubit>()..ensureCartId()..getCartItems()),
-
         BlocProvider(create: (context) => BottomNavCubit()),
         BlocProvider(create: (context) => ThemeCubit()..init()),
-        BlocProvider(create: (context) => sl<ProductDetailsCubit>()),
         BlocProvider(create: (context) => sl<HomeCubit>()..getProduct()),
         BlocProvider(create: (context) => sl<AuthCubit>()..getProfile()),
         BlocProvider(create: (context) => sl<CategoryCubit>()..getCategories(context)),
-        // BlocProvider(create: (context) => sl<OrderCubit>()),
-        // BlocProvider(create: (context) => sl<ShippingCubit>()),
-        //  BlocProvider(create: (context) => sl<PaymentCubit>()),
-        // BlocProvider(create: (context) => sl<AddressCubit>()..getAddresses()),
+
       ],
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, themeMode) {
-         // context.read<BottomNavCubit>().changeBottomNavIndex(0);
           return MaterialApp(
             title: 'My Store',
             theme: AppTheme.lightTheme(),
             darkTheme: AppTheme.darkTheme(),
             themeMode: context.read<ThemeCubit>().themeMode(),
             onGenerateRoute: AppRoute.generateRoute,
-            initialRoute:
-                // Routers.success
-                //
-                AppConstants.token == null || AppConstants.token == ''
+            initialRoute: AppConstants.token == null ||
+                AppConstants.token == ''
                 ? Routers.login
                 : Routers.bottomNav,
           );
