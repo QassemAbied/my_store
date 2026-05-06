30430e4b77bbc65f739604af02050bc1b127c3ab
qassem abied <qassemab1@gmail.com>
2026-05-06 13:39:55 +0300

    add ProductVariantEntities and include variants list in ProductEntities domain model

--- lib/features/products/domain/entities/product_details_entities.dart
+++ lib/features/products/domain/entities/product_details_entities.dart
@@ -1,8 +1,7 @@
 class ProductDetailsEntities {
   final ProductEntities product;
 
-  ProductDetailsEntities({ required this.product});
-
+  ProductDetailsEntities({required this.product});
 }
 
 class ProductEntities {
@@ -10,56 +9,68 @@ class ProductEntities {
   final String title;
   final String description;
   final String thumbnail;
-  final CollectionEntities collection;
+  final CollectionEntities? collection;
   final List<CategoryEntities> categories;
   final List<ImageEntities> images;
   final List<OptionEntities> options;
+  final List<ProductVariantEntities> variants;
 
   ProductEntities({
-     required this.id,
-     required this.title,
-     required this.description,
-     required this.thumbnail,
-     required this.collection,
-     required this.categories,
-     required this.images,
-     required this.options,
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.thumbnail,
+    required this.collection,
+    required this.categories,
+    required this.images,
+    required this.options,
+    required this.variants,
   });
-
 }
 
 class CollectionEntities {
   final String title;
 
-  CollectionEntities({ required this.title});
-
+  CollectionEntities({required this.title});
 }
+
 class CategoryEntities {
   final String name;
 
-  CategoryEntities({ required this.name});
-
+  CategoryEntities({required this.name});
 }
+
 class ImageEntities {
   final String url;
 
-  ImageEntities({ required this.url});
-
+  ImageEntities({required this.url});
 }
 
 class OptionEntities {
   final String title;
   final List<ValueEntities> values;
 
-  OptionEntities({
-    required this.title,
-    required this.values,
-  });
-
+  OptionEntities({required this.title, required this.values});
 }
-  class ValueEntities {
+
+class ValueEntities {
   final String value;
 
   ValueEntities({required this.value});
+}
 
-  }
+class ProductVariantEntities {
+  final String id;
+  final String title;
+  final String sku;
+  final bool manageInventory;
+  final List<ValueEntities> options;
+
+  ProductVariantEntities({
+    required this.id,
+    required this.title,
+    required this.sku,
+    required this.manageInventory,
+    required this.options,
+  });
+}
