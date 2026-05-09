9702c605c786e2be827d7a98b369a5fb02bdd978
qassem abied <qassemab1@gmail.com>
2026-05-10 00:26:53 +0300

    update Failure and Exception classes to support descriptive messages and add specific error types

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,11 +1,29 @@
 abstract class Failure {
-  const Failure();
+  final String message;
+
+  const Failure(this.message);
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure();
+  const ServerFailure(super.message);
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure();
+  const CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure(super.message);
+}
+
+class UnauthorizedFailure extends Failure {
+  const UnauthorizedFailure(super.message);
+}
+
+class NotFoundFailure extends Failure {
+  const NotFoundFailure(super.message);
+}
+
+class BadRequestFailure extends Failure {
+  const BadRequestFailure(super.message);
 }
