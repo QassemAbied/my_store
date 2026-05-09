00228891f250c7d642d2ecc48470f7223c609e0a
qassem abied <qassemab1@gmail.com>
2026-05-10 00:24:12 +0300

    create ErrorMessages class to centralize standard error message strings

---
+++ lib/core/error/err_message.dart
@@ -0,0 +1,19 @@
+class ErrorMessages {
+  static const String noInternet =
+      'No internet connection';
+
+  static const String serverError =
+      'Something went wrong';
+
+  static const String timeout =
+      'Connection timeout';
+
+  static const String unauthorized =
+      'Unauthorized access';
+
+  static const String notFound =
+      'Resource not found';
+
+  static const String badRequest =
+      'Bad request';
+}
