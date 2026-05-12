72434011ffb9a511ec6856338f73801503d4b6e0
qassem abied <qassemab1@gmail.com>
2026-05-12 14:49:09 +0300

    move remote_data_source and its implementation to a dedicated remote_data_source directory within the home feature

--- lib/features/home/data/data_source/remote_data_source/remote_data_source.dart
+++ lib/features/home/data/data_source/remote_data_source/remote_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:my_store/core/common_models/models/product_models.dart';
-import '../../domain/entities/product_param.dart';
+import '../../../domain/entities/product_param.dart';
 
 abstract class RemoteDataSource {
   Future<ProductResponseModel> getProducts(ProductParams params);
