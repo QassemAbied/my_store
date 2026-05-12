ef5a8d9f1a1e48605e3eedda8ae0fbdcf03b4fa2
qassem abied <qassemab1@gmail.com>
2026-05-12 14:26:33 +0300

    create CategoryLocalModel and CategoryResponseLocalModel for local category data storage using Hive

---
+++ lib/features/category/data/models/category_local_model.dart
@@ -0,0 +1,41 @@
+import 'package:hive_ce_flutter/adapters.dart';
+
+part 'category_local_model.g.dart';
+
+@HiveType(typeId: 10)
+class CategoryResponseLocalModel extends HiveObject {
+
+  @HiveField(0)
+  final List<CategoryLocalModel> categories;
+
+  CategoryResponseLocalModel({
+    required this.categories,
+  });
+}
+
+@HiveType(typeId: 11)
+class CategoryLocalModel extends HiveObject {
+
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String name;
+
+  @HiveField(2)
+  final String? description;
+
+  @HiveField(3)
+  final String handle;
+
+  @HiveField(4)
+  final String? parentCategoryId;
+
+  CategoryLocalModel({
+    required this.id,
+    required this.name,
+    this.description,
+    required this.handle,
+    this.parentCategoryId,
+  });
+}
---
+++ lib/features/category/data/models/category_local_model.g.dart
@@ -0,0 +1,88 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category_local_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CategoryResponseLocalModelAdapter
+    extends TypeAdapter<CategoryResponseLocalModel> {
+  @override
+  final typeId = 10;
+
+  @override
+  CategoryResponseLocalModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CategoryResponseLocalModel(
+      categories: (fields[0] as List).cast<CategoryLocalModel>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CategoryResponseLocalModel obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.categories);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryResponseLocalModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class CategoryLocalModelAdapter extends TypeAdapter<CategoryLocalModel> {
+  @override
+  final typeId = 11;
+
+  @override
+  CategoryLocalModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CategoryLocalModel(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      description: fields[2] as String?,
+      handle: fields[3] as String,
+      parentCategoryId: fields[4] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CategoryLocalModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
+      ..write(obj.handle)
+      ..writeByte(4)
+      ..write(obj.parentCategoryId);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryLocalModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
