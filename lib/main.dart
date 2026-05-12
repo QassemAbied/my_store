d4220fc8542e55ba428fbc7dbfd09b7566f09c47
qassem abied <qassemab1@gmail.com>
2026-05-12 14:51:10 +0300

    initialize Hive, register adapters, and open boxes for product and category local storage in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:my_store/core/services/shared_pref.dart';
 import 'package:my_store/core/theme/app_theme.dart';
 import 'package:my_store/core/theme/theme_controller/theme_state.dart';
@@ -12,16 +13,26 @@ import 'package:my_store/core/utils/routing/routers.dart';
 import 'package:my_store/features/cart/presentation/cubit/cart_cubit.dart';
 import 'package:my_store/features/category/presentation/controller/category_cubit.dart';
 import 'package:my_store/features/home/presentation/cubit/home_cubit.dart';
+import 'core/common_models/models/product_response_local_model.dart';
 import 'core/utils/constants.dart';
 import 'features/auth/presentation/cubit/auth_cubit.dart';
 import 'features/bottom_nav_bar/controller/bottom_nav_cubit.dart';
 import 'package:my_store/core/theme/theme_controller/theme_cubit.dart';
-
+import 'features/category/data/models/category_local_model.dart';
 import 'firebase_options.dart';
 import 'injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+ await Hive.initFlutter();
+ Hive.registerAdapter(ProductResponseLocalModelAdapter());
+  Hive.registerAdapter(ProductItemModelsAdapter());
+  Hive.registerAdapter(VariantModelAdapter());
+  Hive.registerAdapter(CalculatedPriceModelAdapter());
+ await  Hive.openBox<ProductResponseLocalModel>(AppConstants.productLocalKey);
+  Hive.registerAdapter(CategoryResponseLocalModelAdapter());
+  Hive.registerAdapter(CategoryLocalModelAdapter());
+  await  Hive.openBox<CategoryResponseLocalModel>(AppConstants.categoryLocalKey);
   Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -29,6 +40,9 @@ void main() async {
   await dotenv.load(fileName: ".env");
   Stripe.publishableKey =dotenv.env['STRIPE_PUBLISHABLE_KEY']!;
 
+//Hive.box<ProductResponseLocalModel>(
+    //  AppConstants.productLocalKey).clear();
+ // Hive.box<CategoryResponseLocalModel>(AppConstants.categoryLocalKey).clear();
   await Stripe.instance.applySettings();
   await SharedPrefHelper.init();
   if (kDebugMode) {
