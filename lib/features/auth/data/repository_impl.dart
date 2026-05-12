7490d4852a4cf80fc7207b8c14e30123bed68e7b
qassem abied <qassemab1@gmail.com>
2026-05-13 00:07:11 +0300

    implement caching and offline support for profile retrieval in AuthRepositoryImpl

--- lib/features/auth/data/repository_impl.dart
+++ lib/features/auth/data/repository_impl.dart
@@ -1,17 +1,26 @@
+import 'package:my_store/core/network/network_info.dart';
+import 'package:my_store/features/auth/data/data_source/local_data_source/auth_local_data_source.dart';
 import 'package:my_store/features/auth/domain/entities/customer_entity.dart';
 import '../../../core/network/api_result.dart';
 import '../../../core/error/error_handler.dart';
 import '../domain/entities/auth_entity.dart';
 import '../domain/entities/requests.dart';
 import '../domain/mapper/auth_mapper.dart';
+import '../domain/mapper/customer_local_mapper.dart';
 import '../domain/mapper/customer_mapper.dart';
 import '../domain/repository.dart';
-import 'data_source/auth_remote_data_source.dart';
+import 'data_source/remote_data_source/auth_remote_data_source.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
+  final NetworkInfo _networkInfo;
+  final AuthLocalDataSource _localDataSource;
 
-  AuthRepositoryImpl({required this.remoteDataSource});
+  AuthRepositoryImpl(
+     this.remoteDataSource,
+     this._networkInfo,
+     this._localDataSource
+  );
 
   @override
   Future<ApiResult<AuthEntity>> registerAuth(LoginRequest request)
@@ -49,12 +58,34 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<ApiResult<CustomerEntity>> getProfile( ) async{
-    try {
-      final result = await remoteDataSource.getProfile();
 
-      return ApiResult.success(result.toEntity());
-    } catch (e) {
-      return ApiResult.failure(ErrorHandler.handle(e));
+    if(await _networkInfo.isServerAlive){
+      try {
+        final result = await remoteDataSource.getProfile();
+        final customerLocal= CustomerLocalMapper.toLocalCustomerModel(result);
+
+        await _localDataSource.cacheProfile(customerLocal);
+
+        return ApiResult.success(result.toEntity());
+      } catch (e) {
+        try{
+          final localProfile= await _localDataSource.getLocalProfile();
+          final entity= CustomerLocalMapper.toLocalEntity(localProfile);
+          return ApiResult.success(entity);
+        }catch(_){
+          return ApiResult.failure(ErrorHandler.handle(e));
+        }
+
+      }
+    }else{
+      try{
+        final localProfile= await _localDataSource.getLocalProfile();
+        final entity= CustomerLocalMapper.toLocalEntity(localProfile);
+        return ApiResult.success(entity);
+      }catch(e){
+        return ApiResult.failure(ErrorHandler.handle(e));
+      }
     }
+
   }
 }
