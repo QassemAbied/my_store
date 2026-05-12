499c776a9a5234e558516d88542fb00cc4e7d4ab
qassem abied <qassemab1@gmail.com>
2026-05-12 14:48:53 +0300

    move category remote data source files to remote_data_source directory

--- lib/features/category/data/data_source/remote_data_source/category_remote_data_source.dart
+++ lib/features/category/data/data_source/remote_data_source/category_remote_data_source.dart
@@ -1,6 +1,6 @@
-import '../../../../core/common_models/models/product_models.dart';
-import '../../domain/entities/product_by_category_request.dart';
-import '../models/category_model.dart';
+import '../../../../../core/common_models/models/product_models.dart';
+import '../../../domain/entities/product_by_category_request.dart';
+import '../../models/category_model.dart';
 
 abstract class CategoryRemoteDataSource {
   Future<CategoryResponseModel> getCategories();
