433c8bfe7e4842fd12bc1278c81aac0d822111e1
qassem abied <qassemab1@gmail.com>
2026-05-12 14:50:11 +0300

    add category and product local storage keys to constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,4 +4,7 @@ class AppConstants {
   static const String themeKey='theme';
   static const String tokenKey='TOKEN';
   static String? token=SharedPrefHelper.getString(key: tokenKey);
+  static const String categoryLocalKey='categorys';
+  static const String productLocalKey='productes';
+
 }
