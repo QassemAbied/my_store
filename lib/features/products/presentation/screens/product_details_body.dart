1a52852d10a4d752c37ae8e95f95f22a348db143
qassem abied <qassemab1@gmail.com>
2026-05-20 03:47:15 +0300

    update ProductDetailsBody to include "You may also like" section and RecommendationProductWidget

--- lib/features/products/presentation/screens/product_details_body.dart
+++ lib/features/products/presentation/screens/product_details_body.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:my_store/core/theme/app_colors.dart';
 import 'package:my_store/features/products/presentation/screens/widgets/component_details_widget.dart';
 import 'package:my_store/features/products/presentation/screens/widgets/image_slider_widget.dart';
+import 'package:my_store/features/products/presentation/screens/widgets/recommendation_product_widget.dart';
+import '../../../../core/theme/color_extension.dart';
+import '../../../../core/utils/app_text_style.dart';
+import '../../../../core/utils/spacing.dart';
 import '../../domain/entities/product_details_entities.dart';
 
 class ProductDetailsBody extends StatelessWidget {
   const ProductDetailsBody({super.key, required this.product});
+
   final ProductDetailsEntities product;
 
   @override
@@ -22,9 +27,31 @@ class ProductDetailsBody extends StatelessWidget {
           ),
         ),
 
+        SliverToBoxAdapter(child: ComponentDetailsWidget(product: product)),
         SliverToBoxAdapter(
-          child: ComponentDetailsWidget(product: product),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+
+            child: Row(
+              children: [
+                Icon(Icons.auto_awesome, color: context.primaryColor),
+
+                horizontalSpace(8),
+
+                Text(
+                  'You may also like',
+
+                  style: AppTextStyle.bold(
+                    fontSize: 20,
+
+                    color: context.textPrimary,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
+        SliverToBoxAdapter(child: RecommendationProductWidget()),
       ],
     );
   }
